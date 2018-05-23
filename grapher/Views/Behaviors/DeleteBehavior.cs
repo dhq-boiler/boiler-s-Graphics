5a67c84c12d98ede78ea048e65c7069c75ab4341
dhq_boiler <dhq_boiler@live.jp>
2018-05-23 12:33:15 +0900

    Implement function that user pressed delete key then, selected render item is eliminated

---
+++ grapher/Views/Behaviors/DeleteBehavior.cs
@@ -0,0 +1,39 @@
+﻿using grapher.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace grapher.Views.Behaviors
+{
+    class DeleteBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.PreviewKeyDown += AssociatedObject_PreviewKeyDown;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.PreviewKeyDown -= AssociatedObject_PreviewKeyDown;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Delete)
+            {
+                var mainwindowViewModel = (App.Current.MainWindow.DataContext as MainWindowViewModel);
+                var removes = mainwindowViewModel.RenderItems.Where(i => i.IsSelected.Value).ToList();
+                foreach (var viewModel in removes)
+                {
+                    mainwindowViewModel.RenderItems.Remove(viewModel);
+                }
+            }
+        }
+    }
+}
