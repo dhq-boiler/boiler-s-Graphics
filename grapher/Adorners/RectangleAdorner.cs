2b3bff54c010d83f533e7dd0352a1038d896b352
dhq_boiler <dhq_boiler@live.jp>
2018-06-25 00:39:24 +0900

    Randomize stroke color of the rectangle

--- grapher/Adorners/RectangleAdorner.cs
+++ grapher/Adorners/RectangleAdorner.cs
@@ -1,7 +1,8 @@
 ﻿using grapher.Controls;
 using grapher.ViewModels;
 using System;
-using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -56,18 +57,16 @@ namespace grapher.Adorners
 
             if (_startPoint.HasValue && _endPoint.HasValue)
             {
+                var rand = new Random();
                 var item = new NRectangleViewModel();
                 item.Left = Math.Min(_startPoint.Value.X, _endPoint.Value.X);
                 item.Top = Math.Min(_startPoint.Value.Y, _endPoint.Value.Y);
                 item.Width = Math.Max(_startPoint.Value.X - _endPoint.Value.X, _endPoint.Value.X - _startPoint.Value.X);
                 item.Height = Math.Max(_startPoint.Value.Y - _endPoint.Value.Y, _endPoint.Value.Y - _startPoint.Value.Y);
-                item.EdgeColor = new Color()
-                {
-                    A = 255,
-                    R = (byte)new Random().Next(255),
-                    G = (byte)new Random().Next(255),
-                    B = (byte)new Random().Next(255),
-                };
+                item.EdgeColor = typeof(Colors).GetProperties(BindingFlags.Static | BindingFlags.Public)
+                    .Select(x => (Color)x.GetValue(typeof(Colors), null))
+                    .OrderBy(x => rand.Next())
+                    .FirstOrDefault();
                 ((AdornedElement as DesignerCanvas).DataContext as IDiagramViewModel).AddItemCommand.Execute(item);
 
                 _startPoint = null;
