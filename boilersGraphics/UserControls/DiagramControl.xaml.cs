d7f9cb44bb925b110c345cc3653bfa33523f60d4
dhq_boiler <dhq_boiler@live.jp>
2021-07-03 23:19:00 +0900

    merge

--- boilersGraphics/UserControls/DiagramControl.xaml.cs
+++ boilersGraphics/UserControls/DiagramControl.xaml.cs
@@ -26,10 +26,10 @@ namespace boilersGraphics.UserControls
             InitializeComponent();
         }
 
-        //private void DesignerCanvas_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    DesignerCanvas myDesignerCanvas = sender as DesignerCanvas;
-        //    zoomBox.DesignerCanvas = myDesignerCanvas;
-        //}
+        private void DesignerCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            DesignerCanvas myDesignerCanvas = sender as DesignerCanvas;
+            zoomBox.DesignerCanvas = myDesignerCanvas;
+        }
     }
 }
--- boilersGraphics/ViewModels/DiagramViewModel.cs
+++ boilersGraphics/ViewModels/DiagramViewModel.cs
@@ -32,8 +32,6 @@ namespace boilersGraphics.ViewModels
         private ObservableCollection<Color> _EdgeColors = new ObservableCollection<Color>();
         private ObservableCollection<Color> _FillColors = new ObservableCollection<Color>();
         private CompositeDisposable _CompositeDisposable = new CompositeDisposable();
-        private int _Width;
-        private int _Height;
         private double _BorderThickness;
         private bool _MiddleButtonIsPressed;
         private Point _MousePointerPosition;
@@ -268,18 +266,6 @@ namespace boilersGraphics.ViewModels
         {
             get { return _FillColors; }
             set { SetProperty(ref _FillColors, value); }
-        }
-
-        public int Width
-        {
-            get { return _Width; }
-            set { SetProperty(ref _Width, value); }
-        }
-
-        public int Height
-        {
-            get { return _Height; }
-            set { SetProperty(ref _Height, value); }
         }
 
         public void DeselectAll()
