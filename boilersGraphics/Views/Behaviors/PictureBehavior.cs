2422fdb09ae5af04306c328bcc52d4c9f5638efd
dhq_boiler <dhq_boiler@live.jp>
2021-07-05 21:49:54 +0900

    implemented a function to snap at the beginning and ending of drawing a picture

--- boilersGraphics/Views/Behaviors/PictureBehavior.cs
+++ boilersGraphics/Views/Behaviors/PictureBehavior.cs
@@ -9,6 +9,9 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Documents;
 using boilersGraphics.Adorners;
+using boilersGraphics.Extensions;
+using boilersGraphics.ViewModels;
+using System.Diagnostics;
 
 namespace boilersGraphics.Views.Behaviors
 {
@@ -18,11 +21,13 @@ namespace boilersGraphics.Views.Behaviors
         private string _filename;
         private double _Width;
         private double _Height;
+        private Dictionary<Point, Adorner> _adorners;
         public PictureBehavior(string filename, double width, double height)
         {
             _filename = filename;
             _Width = width;
             _Height = height;
+            _adorners = new Dictionary<Point, Adorner>();
         }
 
         protected override void OnAttached()
@@ -44,6 +49,52 @@ namespace boilersGraphics.Views.Behaviors
             var canvas = AssociatedObject as DesignerCanvas;
             if (canvas.SourceConnector == null)
             {
+                var mainWindowVM = (App.Current.MainWindow.DataContext as MainWindowViewModel);
+                var designerCanvas = App.Current.MainWindow.GetChildOfType<DesignerCanvas>();
+                var diagramVM = mainWindowVM.DiagramViewModel;
+                var snapPoints = diagramVM.SnapPoints;
+                Point? snapped = null;
+                var currentPoint = e.GetPosition(AssociatedObject);
+                foreach (var snapPoint in snapPoints)
+                {
+                    if (currentPoint.X > snapPoint.X - mainWindowVM.SnapPower.Value
+                     && currentPoint.X < snapPoint.X + mainWindowVM.SnapPower.Value
+                     && currentPoint.Y > snapPoint.Y - mainWindowVM.SnapPower.Value
+                     && currentPoint.Y < snapPoint.Y + mainWindowVM.SnapPower.Value)
+                    {
+                        //スナップする座標を一時変数へ保存
+                        snapped = snapPoint;
+                    }
+                }
+
+                if (snapped != null)
+                {
+                    AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(designerCanvas);
+                    RemoveFromAdornerLayerAndDictionary(snapped, adornerLayer);
+
+                    //ドラッグ終了座標を一時変数で上書きしてスナップ
+                    _pictureDrawingStartPoint = snapped;
+                    if (adornerLayer != null)
+                    {
+                        Trace.WriteLine($"Snap={snapped.Value}");
+                        if (!_adorners.ContainsKey(snapped.Value))
+                        {
+                            var adorner = new Adorners.SnapPointAdorner(designerCanvas, snapped.Value);
+                            if (adorner != null)
+                            {
+                                adornerLayer.Add(adorner);
+
+                                //ディクショナリに記憶する
+                                _adorners.Add(snapped.Value, adorner);
+                            }
+                        }
+                    }
+                }
+                else //スナップしなかった場合
+                {
+                    RemoveAllAdornerFromAdornerLayerAndDictionary(designerCanvas);
+                }
+
                 if (e.LeftButton != MouseButtonState.Pressed)
                     _pictureDrawingStartPoint = null;
 
@@ -61,6 +112,35 @@ namespace boilersGraphics.Views.Behaviors
                 }
             }
             e.Handled = true;
+        }
+
+        private void RemoveAllAdornerFromAdornerLayerAndDictionary(DesignerCanvas designerCanvas)
+        {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(designerCanvas);
+            var removes = _adorners.ToList();
+
+            removes.ForEach(x =>
+            {
+                if (adornerLayer != null)
+                {
+                    adornerLayer.Remove(x.Value);
+                }
+                _adorners.Remove(x.Key);
+            });
+        }
+
+        private void RemoveFromAdornerLayerAndDictionary(Point? snapped, AdornerLayer adornerLayer)
+        {
+            var removes = _adorners.Where(x => x.Key != snapped)
+                                                        .ToList();
+            removes.ForEach(x =>
+            {
+                if (adornerLayer != null)
+                {
+                    adornerLayer.Remove(x.Value);
+                }
+                _adorners.Remove(x.Key);
+            });
         }
 
         private void AssociatedObject_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
