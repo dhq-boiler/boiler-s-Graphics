3f0906058bc8be9e0685e14421349c1657e14c8c
dhq_boiler <dhq_boiler@live.jp>
2021-08-02 20:14:28 +0900

    bugfix (incomplete)

--- boilersGraphics/Models/Layer.cs
+++ boilersGraphics/Models/Layer.cs
@@ -72,11 +72,28 @@ namespace boilersGraphics.Models
                  .Subscribe(x =>
                  {
                      Trace.WriteLine("detected Layer changes. run Layer.UpdateAppearance().");
-                     UpdateAppearance(Children.Select(xx => (xx as LayerItem).Item.Value));
-                     Children.ToList().ForEach(x => (x as LayerItem).UpdateAppearance((x as LayerItem).Item.Value));
+                     UpdateAppearance(Children.SelectRecursive<LayerTreeViewItemBase, LayerTreeViewItemBase>(xx => xx.Children).Select(x => (x as LayerItem).Item.Value));
+                     Children.SelectRecursive<LayerTreeViewItemBase, LayerTreeViewItemBase>(x => x.Children)
+                             .ToList()
+                             .ForEach(x => (x as LayerItem).UpdateAppearance(IfGroupBringChildren((x as LayerItem).Item.Value)));
                  })
                  .AddTo(_disposable);
             IsVisible.Value = true;
+        }
+
+        private IEnumerable<SelectableDesignerItemViewModelBase> IfGroupBringChildren(SelectableDesignerItemViewModelBase value)
+        {
+            if (value is GroupItemViewModel groupItemVM)
+            {
+                var children = Children.SelectRecursive<LayerTreeViewItemBase, LayerTreeViewItemBase>(x => x.Children)
+                                       .Select(x => (x as LayerItem).Item.Value)
+                                       .Where(x => x.ParentID == groupItemVM.ID);
+                return children;
+            }
+            else
+            {
+                return new List<SelectableDesignerItemViewModelBase>() { value };
+            }
         }
 
         public IObservable<Unit> LayerItemsChangedAsObservable()
@@ -110,44 +127,49 @@ namespace boilersGraphics.Models
 
             foreach (var item in items)
             {
-                var views = designerCanvas.GetCorrespondingViews<FrameworkElement>(item).Where(x => x.GetType() == item.GetViewType());
-                foreach (var view in views)
-                {
-                    if (view != null)
-                    {
-                        if (view.ActualWidth >= 1 && view.ActualHeight >= 1)
-                        {
-                            using (DrawingContext context = visual.RenderOpen())
-                            {
-                                double diffX = 0;
-                                double diffY = 0;
-                                var designerItem = item as DesignerItemViewModelBase;
-                                var connectorItem = item as ConnectorBaseViewModel;
-                                if (designerItem != null)
-                                {
-                                    diffX = designerItem.Left.Value - minX;
-                                    diffY = designerItem.Top.Value - minY;
-                                }
-                                if (connectorItem != null)
-                                {
-                                    diffX = Math.Min(connectorItem.Points[0].X, connectorItem.Points[1].X);
-                                    diffY = Math.Min(connectorItem.Points[0].Y, connectorItem.Points[1].Y);
-                                }
-                                VisualBrush brush = new VisualBrush(view);
-                                context.DrawRectangle(brush, null, new Rect(new Point(diffX, diffY), new Size(view.ActualWidth, view.ActualHeight)));
-                            }
-
-                            rtb.Render(visual);
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("view not found");
-                    }
-                }
+                UpdateAppearanceByItem(designerCanvas, minX, minY, rtb, visual, item);
             }
 
             Appearance.Value = rtb;
+        }
+
+        private static void UpdateAppearanceByItem(DesignerCanvas designerCanvas, double minX, double minY, RenderTargetBitmap rtb, DrawingVisual visual, SelectableDesignerItemViewModelBase item)
+        {
+            var views = designerCanvas.GetCorrespondingViews<FrameworkElement>(item).Where(x => x.GetType() == item.GetViewType());
+            foreach (var view in views)
+            {
+                if (view != null)
+                {
+                    if (view.ActualWidth >= 1 && view.ActualHeight >= 1)
+                    {
+                        using (DrawingContext context = visual.RenderOpen())
+                        {
+                            double diffX = 0;
+                            double diffY = 0;
+                            var designerItem = item as DesignerItemViewModelBase;
+                            var connectorItem = item as ConnectorBaseViewModel;
+                            if (designerItem != null)
+                            {
+                                diffX = designerItem.Left.Value - minX;
+                                diffY = designerItem.Top.Value - minY;
+                            }
+                            if (connectorItem != null)
+                            {
+                                diffX = Math.Min(connectorItem.Points[0].X, connectorItem.Points[1].X);
+                                diffY = Math.Min(connectorItem.Points[0].Y, connectorItem.Points[1].Y);
+                            }
+                            VisualBrush brush = new VisualBrush(view);
+                            context.DrawRectangle(brush, null, new Rect(new Point(diffX, diffY), new Size(view.ActualWidth, view.ActualHeight)));
+                        }
+
+                        rtb.Render(visual);
+                    }
+                }
+                else
+                {
+                    throw new Exception("view not found");
+                }
+            }
         }
 
         private static DrawingVisual InitializeBitmap(int width, int height)
--- boilersGraphics/Models/LayerItem.cs
+++ boilersGraphics/Models/LayerItem.cs
@@ -4,6 +4,7 @@ using boilersGraphics.ViewModels;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Windows;
@@ -61,9 +62,31 @@ namespace boilersGraphics.Models
             IsVisible.Value = true;
         }
 
-        public void UpdateAppearance(SelectableDesignerItemViewModelBase item)
+        public void UpdateAppearance(IEnumerable<SelectableDesignerItemViewModelBase> items)
         {
             var designerCanvas = App.Current.MainWindow.GetChildOfType<DesignerCanvas>();
+            double minX, maxX, minY, maxY;
+            var width = GetWidth(items, out minX, out maxX);
+            var height = GetHeight(items, out minY, out maxY);
+
+            if (width < 0 || height < 0)
+                return;
+
+            var rtb = new RenderTargetBitmap((int)width, (int)height, 96, 96, PixelFormats.Pbgra32);
+
+            DrawingVisual visual = InitializeBitmap(width, height);
+            rtb.Render(visual);
+
+            foreach (var item in items)
+            {
+                UpdateAppearanceByItem(designerCanvas, minX, minY, rtb, visual, item);
+            }
+
+            Appearance.Value = rtb;
+        }
+
+        private static void UpdateAppearanceByItem(DesignerCanvas designerCanvas, double minX, double minY, RenderTargetBitmap rtb, DrawingVisual visual, SelectableDesignerItemViewModelBase item)
+        {
             var views = designerCanvas.GetCorrespondingViews<FrameworkElement>(item).Where(x => x.GetType() == item.GetViewType());
             foreach (var view in views)
             {
@@ -71,20 +94,27 @@ namespace boilersGraphics.Models
                 {
                     if (view.ActualWidth >= 1 && view.ActualHeight >= 1)
                     {
-                        var rtb = new RenderTargetBitmap((int)view.ActualWidth, (int)view.ActualHeight, 96, 96, PixelFormats.Pbgra32);
-
-                        DrawingVisual visual = new DrawingVisual();
                         using (DrawingContext context = visual.RenderOpen())
                         {
-                            context.DrawRectangle(Brushes.White, null, new Rect(new Point(), new Size(view.ActualWidth, view.ActualHeight)));
-
+                            double diffX = 0;
+                            double diffY = 0;
+                            var designerItem = item as DesignerItemViewModelBase;
+                            var connectorItem = item as ConnectorBaseViewModel;
+                            if (designerItem != null)
+                            {
+                                diffX = designerItem.Left.Value - minX;
+                                diffY = designerItem.Top.Value - minY;
+                            }
+                            if (connectorItem != null)
+                            {
+                                diffX = Math.Min(connectorItem.Points[0].X, connectorItem.Points[1].X);
+                                diffY = Math.Min(connectorItem.Points[0].Y, connectorItem.Points[1].Y);
+                            }
                             VisualBrush brush = new VisualBrush(view);
-                            context.DrawRectangle(brush, null, new Rect(new Point(), new Size(view.ActualWidth, view.ActualHeight)));
+                            context.DrawRectangle(brush, null, new Rect(new Point(diffX, diffY), new Size(view.ActualWidth, view.ActualHeight)));
                         }
 
                         rtb.Render(visual);
-
-                        Appearance.Value = rtb;
                     }
                 }
                 else
@@ -92,6 +122,61 @@ namespace boilersGraphics.Models
                     throw new Exception("view not found");
                 }
             }
+        }
+
+        private static DrawingVisual InitializeBitmap(int width, int height)
+        {
+            DrawingVisual visual = new DrawingVisual();
+            using (DrawingContext context = visual.RenderOpen())
+            {
+                //白背景でビットマップを初期化
+                context.DrawRectangle(Brushes.White, null, new Rect(new Point(), new Size(width, height)));
+            }
+            return visual;
+        }
+
+        private int GetWidth(IEnumerable<SelectableDesignerItemViewModelBase> items, out double minX, out double maxX)
+        {
+            minX = 0d;
+            maxX = 0d;
+            foreach (var item in items)
+            {
+                var desingerItem = item as DesignerItemViewModelBase;
+                var connectorItem = item as ConnectorBaseViewModel;
+                if (desingerItem != null)
+                {
+                    minX = Math.Min(Math.Min(minX, desingerItem.Left.Value), desingerItem.Right.Value);
+                    maxX = Math.Max(Math.Max(maxX, desingerItem.Left.Value), desingerItem.Right.Value);
+                }
+                if (connectorItem != null)
+                {
+                    minX = Math.Min(Math.Min(minX, connectorItem.Points[0].X), connectorItem.Points[1].X);
+                    maxX = Math.Max(Math.Max(maxX, connectorItem.Points[0].X), connectorItem.Points[1].X);
+                }
+            }
+            return (int)(maxX - minX);
+        }
+
+        private int GetHeight(IEnumerable<SelectableDesignerItemViewModelBase> items, out double minY, out double maxY)
+        {
+            minY = 0d;
+            maxY = 0d;
+            foreach (var item in items)
+            {
+                var desingerItem = item as DesignerItemViewModelBase;
+                var connectorItem = item as ConnectorBaseViewModel;
+                if (desingerItem != null)
+                {
+                    minY = Math.Min(Math.Min(minY, desingerItem.Top.Value), desingerItem.Bottom.Value);
+                    maxY = Math.Max(Math.Max(maxY, desingerItem.Top.Value), desingerItem.Bottom.Value);
+                }
+                if (connectorItem != null)
+                {
+                    minY = Math.Min(Math.Min(minY, connectorItem.Points[0].X), connectorItem.Points[1].X);
+                    maxY = Math.Max(Math.Max(maxY, connectorItem.Points[0].X), connectorItem.Points[1].X);
+                }
+            }
+            return (int)(maxY - minY);
         }
 
         public override string ToString()
