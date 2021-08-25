096d1c3c6c56a1ec90a7771dac92edd3a2203817
dhq_boiler <dhq_boiler@live.jp>
2021-08-25 17:44:35 +0900

    implement version window

---
+++ boilersGraphics/ViewModels/VersionViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boilersGraphics.ViewModels
+{
+    public class VersionViewModel : BindableBase, IDialogAware
+    {
+        private CompositeDisposable _disposables = new CompositeDisposable();
+        public string Title => "boiler's Graphics のバージョン情報";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public ReactivePropertySlim<string> Version { get; } = new ReactivePropertySlim<string>();
+
+        public ReactiveCommand OKCommand { get; } = new ReactiveCommand();
+
+        public VersionViewModel()
+        {
+            Version.Value = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+            OKCommand.Subscribe(() =>
+            {
+                DialogResult result = new DialogResult(ButtonResult.OK);
+                RequestClose.Invoke(result);
+            })
+            .AddTo(_disposables);
+        }
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
