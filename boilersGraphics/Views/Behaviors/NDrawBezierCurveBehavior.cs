66833fcddc4e13236c9ba3c749a12d97a7a886ca
dhq_boiler <dhq_boiler@live.jp>
2021-09-01 21:12:43 +0900

    support stylus

--- boilersGraphics/Views/Behaviors/NDrawBezierCurveBehavior.cs
+++ boilersGraphics/Views/Behaviors/NDrawBezierCurveBehavior.cs
@@ -20,6 +20,9 @@ namespace boilersGraphics.Views.Behaviors
 
         protected override void OnAttached()
         {
+            this.AssociatedObject.StylusDown += AssociatedObject_StylusDown;
+            this.AssociatedObject.StylusMove += AssociatedObject_StylusMove;
+            this.AssociatedObject.TouchDown += AssociatedObject_TouchDown;
             this.AssociatedObject.MouseDown += AssociatedObject_MouseDown;
             this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
             this.AssociatedObject.MouseUp += AssociatedObject_MouseUp;
@@ -28,10 +31,31 @@ namespace boilersGraphics.Views.Behaviors
 
         protected override void OnDetaching()
         {
+            this.AssociatedObject.StylusDown -= AssociatedObject_StylusDown;
+            this.AssociatedObject.StylusMove -= AssociatedObject_StylusMove;
+            this.AssociatedObject.TouchDown -= AssociatedObject_TouchDown;
             this.AssociatedObject.MouseDown -= AssociatedObject_MouseDown;
             this.AssociatedObject.MouseMove -= AssociatedObject_MouseMove;
             this.AssociatedObject.MouseUp -= AssociatedObject_MouseUp;
             base.OnDetaching();
+        }
+
+        private void AssociatedObject_StylusDown(object sender, StylusDownEventArgs e)
+        {
+            if (e.Source == AssociatedObject)
+            {
+                _rectangleStartPoint = e.GetPosition(AssociatedObject);
+                e.Handled = true;
+            }
+        }
+
+        private void AssociatedObject_TouchDown(object sender, TouchEventArgs e)
+        {
+            if (e.Source == AssociatedObject)
+            {
+                var touchPoint = e.GetTouchPoint(AssociatedObject);
+                _rectangleStartPoint = touchPoint.Position;
+            }
         }
 
         private void AssociatedObject_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -54,6 +78,32 @@ namespace boilersGraphics.Views.Behaviors
             snapAction.OnMouseMove(ref current);
 
             if (e.LeftButton != MouseButtonState.Pressed)
+                _rectangleStartPoint = null;
+
+            if (_rectangleStartPoint.HasValue)
+            {
+                _rectangleStartPoint = current;
+                (App.Current.MainWindow.DataContext as MainWindowViewModel).CurrentOperation.Value = "描画";
+
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(canvas);
+                if (adornerLayer != null)
+                {
+                    var adorner = new Adorners.BezierCurveAdorner(canvas, _rectangleStartPoint);
+                    if (adorner != null)
+                    {
+                        adornerLayer.Add(adorner);
+                    }
+                }
+            }
+        }
+
+        private void AssociatedObject_StylusMove(object sender, StylusEventArgs e)
+        {
+            var canvas = AssociatedObject as DesignerCanvas;
+            Point current = e.GetPosition(canvas);
+            snapAction.OnMouseMove(ref current);
+
+            if (e.InAir)
                 _rectangleStartPoint = null;
 
             if (_rectangleStartPoint.HasValue)
