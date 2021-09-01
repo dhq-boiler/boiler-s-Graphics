60e112a00a2c5cb8f607f05d03459e0bbebcb641
dhq_boiler <dhq_boiler@live.jp>
2021-09-01 21:24:43 +0900

    support stylus

--- boilersGraphics/Views/Behaviors/NDrawPolygonBehavior.cs
+++ boilersGraphics/Views/Behaviors/NDrawPolygonBehavior.cs
@@ -37,6 +37,9 @@ namespace boilersGraphics.Views.Behaviors
 
         protected override void OnAttached()
         {
+            this.AssociatedObject.StylusDown += AssociatedObject_StylusDown;
+            this.AssociatedObject.StylusMove += AssociatedObject_StylusMove;
+            this.AssociatedObject.TouchDown += AssociatedObject_TouchDown;
             this.AssociatedObject.MouseDown += AssociatedObject_MouseDown;
             this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
             this.AssociatedObject.MouseUp += AssociatedObject_MouseUp;
@@ -45,10 +48,31 @@ namespace boilersGraphics.Views.Behaviors
 
         protected override void OnDetaching()
         {
+            this.AssociatedObject.StylusDown -= AssociatedObject_StylusDown;
+            this.AssociatedObject.StylusMove -= AssociatedObject_StylusMove;
+            this.AssociatedObject.TouchDown -= AssociatedObject_TouchDown;
             this.AssociatedObject.MouseDown -= AssociatedObject_MouseDown;
             this.AssociatedObject.MouseMove -= AssociatedObject_MouseMove;
             this.AssociatedObject.MouseUp -= AssociatedObject_MouseUp;
             base.OnDetaching();
+        }
+
+        private void AssociatedObject_StylusDown(object sender, StylusDownEventArgs e)
+        {
+            if (e.Source == AssociatedObject)
+            {
+                _polygonDrawingStartPoint = e.GetPosition(AssociatedObject);
+                e.Handled = true;
+            }
+        }
+
+        private void AssociatedObject_TouchDown(object sender, TouchEventArgs e)
+        {
+            if (e.Source == AssociatedObject)
+            {
+                var touchPoint = e.GetTouchPoint(AssociatedObject);
+                _polygonDrawingStartPoint = touchPoint.Position;
+            }
         }
 
         private void AssociatedObject_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -71,6 +95,32 @@ namespace boilersGraphics.Views.Behaviors
             snapAction.OnMouseMove(ref current);
 
             if (e.LeftButton != MouseButtonState.Pressed)
+                _polygonDrawingStartPoint = null;
+
+            if (_polygonDrawingStartPoint.HasValue)
+            {
+                _polygonDrawingStartPoint = current;
+                (App.Current.MainWindow.DataContext as MainWindowViewModel).CurrentOperation.Value = "描画";
+
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(canvas);
+                if (adornerLayer != null)
+                {
+                    var adorner = new Adorners.PolygonAdorner(canvas, _polygonDrawingStartPoint, Corners, Data, StartPoint);
+                    if (adorner != null)
+                    {
+                        adornerLayer.Add(adorner);
+                    }
+                }
+            }
+        }
+
+        private void AssociatedObject_StylusMove(object sender, StylusEventArgs e)
+        {
+            var canvas = AssociatedObject as DesignerCanvas;
+            Point current = e.GetPosition(canvas);
+            snapAction.OnMouseMove(ref current);
+
+            if (e.InAir)
                 _polygonDrawingStartPoint = null;
 
             if (_polygonDrawingStartPoint.HasValue)
