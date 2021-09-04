ff5bff6847fce7c7aa66a2d638e52934b46f895c
dhq_boiler <dhq_boiler@live.jp>
2021-09-04 21:13:57 +0900

    add Obsolete attribute

--- boilersGraphics/Converters/ObservableCollectionToPathSegmentCollection.cs
+++ boilersGraphics/Converters/ObservableCollectionToPathSegmentCollection.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 
 namespace boilersGraphics.Converters
 {
+    [Obsolete]
     class ObservableCollectionToPathSegmentCollection : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
