b5bbb54352e8e3eec5a517f0b1d1828adae2ce5f
dhq_boiler <dhq_boiler@live.jp>
2021-09-04 20:49:52 +0900

    add test

--- boilersGraphics.Test/ConverterTest.cs
+++ boilersGraphics.Test/ConverterTest.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Media;
 
 namespace boilersGraphics.Test
@@ -30,6 +31,31 @@ namespace boilersGraphics.Test
         {
             var converter = new ColorToStringConverter();
             Assert.That(() => converter.ConvertBack("#FFFF0000", typeof(string), null, null), Throws.InstanceOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void DoubleToStringConverter_Convert()
+        {
+            var converter = new DoubleToStringConverter();
+            Assert.That(converter.Convert(1d, typeof(double), null, null), Is.EqualTo("1"));
+            Assert.That(converter.Convert(1000d, typeof(double), null, null), Is.EqualTo("1000"));
+            Assert.That(converter.Convert(0.1d, typeof(double), null, null), Is.EqualTo("0.1"));
+            Assert.That(converter.Convert(0.01d, typeof(double), null, null), Is.EqualTo("0.01"));
+            Assert.That(converter.Convert(0.001d, typeof(double), null, null), Is.EqualTo("0.001"));
+            Assert.That(converter.Convert(0.0001d, typeof(double), null, null), Is.EqualTo("0.0001"));
+            Assert.That(converter.Convert(0.00001d, typeof(double), null, null), Is.EqualTo("1E-05"));
+        }
+
+        [Test]
+        public void DoubleToStringConverter_ConvertBack()
+        {
+            var converter = new DoubleToStringConverter();
+            Assert.That(converter.ConvertBack("", typeof(string), null, null), Is.EqualTo(0));
+            Assert.That(converter.ConvertBack("0.", typeof(string), null, null), Is.EqualTo(Binding.DoNothing));
+            Assert.That(converter.ConvertBack("-", typeof(string), null, null), Is.EqualTo(Binding.DoNothing));
+            Assert.That(converter.ConvertBack("0.0001", typeof(string), null, null), Is.EqualTo(0.0001));
+            Assert.That(converter.ConvertBack("0.00001", typeof(string), null, null), Is.EqualTo(0.00001));
+            Assert.That(converter.ConvertBack("0.0001*", typeof(string), null, null), Is.EqualTo(0));
         }
     }
 }
