2f7adef2dcd087ac57e91bc398600f24b4fb95ee
dhq_boiler <dhq_boiler@live.jp>
2021-09-04 21:16:12 +0900

    add Obsolete attribute

--- boilersGraphics/Converters/PointsToAngleConverter.cs
+++ boilersGraphics/Converters/PointsToAngleConverter.cs
@@ -6,6 +6,7 @@ using System.Windows.Data;
 
 namespace boilersGraphics.Converters
 {
+    [Obsolete]
     internal class PointsToAngleConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
