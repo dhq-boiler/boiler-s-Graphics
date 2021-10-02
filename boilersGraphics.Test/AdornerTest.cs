f48e1e97d38b65b91aa31c2f9fc0bdf2963242e1
dhq_boiler <dhq_boiler@live.jp>
2021-10-02 16:20:08 +0900

    add test

---
+++ boilersGraphics.Test/AdornerTest.cs
@@ -0,0 +1,31 @@
+﻿using boilersGraphics.Adorners;
+using boilersGraphics.Controls;
+using boilersGraphics.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace boilersGraphics.Test
+{
+    [TestFixture]
+    public class AdornerTest
+    {
+        [Test, RequiresThread(System.Threading.ApartmentState.STA)]
+        public void BezierCurveAdornerを作成()
+        {
+            var diagramViewModel = new DiagramViewModel(new MainWindowViewModel(null), 100, 100);
+            var designerCanvas = new DesignerCanvas();
+            designerCanvas.DataContext = diagramViewModel;
+            diagramViewModel.EdgeColors.Add(Colors.Red);
+            diagramViewModel.EdgeThickness.Value = 1.0;
+            var adorner = new BezierCurveAdorner(designerCanvas, new System.Windows.Point() { X = 100, Y = 100 });
+            var po = new Microsoft.VisualStudio.TestTools.UnitTesting.PrivateObject(adorner);
+            Assert.That(po.GetField("_designerCanvas"), Is.EqualTo(designerCanvas));
+            Assert.That(po.GetField("_startPoint"), Is.EqualTo(new System.Windows.Point() { X = 100, Y = 100 }));
+        }
+    }
+}
