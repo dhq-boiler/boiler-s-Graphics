206fc41620079ff06dcf6d587606fa3fc45eaa86
dhq_boiler <dhq_boiler@live.jp>
2021-10-13 13:15:25 +0900

    supports Windows 11 snap layout (need to bugfix)

--- boilersGraphics/Views/MainWindow.xaml.cs
+++ boilersGraphics/Views/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace boilersGraphics.Views
 {
@@ -11,7 +15,36 @@ namespace boilersGraphics.Views
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += (o, e) =>
+            {
+                var source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+                source.AddHook(new HwndSourceHook(WndProc));
+            };
         }
+
+        const int WM_NCHITTEST = 0x0084;
+        const int HTMAXBUTTON = 9;
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_NCHITTEST)
+            {
+                int x = lParam.ToInt32() & 0xffff;
+                int y = lParam.ToInt32() >> 16;
+                var maximizeButton = this.MainWindowInstance.Template.FindName("MaximizeButton", this.MainWindowInstance) as Button;
+                var rect = new Rect(maximizeButton.PointToScreen(new Point()), new Size(maximizeButton.Width, maximizeButton.Height));
+                if (rect.Contains(new Point(x, y)))
+                {
+                    handled = true;
+                }
+                return new IntPtr(HTMAXBUTTON);
+            }
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr DefWindowProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
 
         private void CommandBinding_CanExecute_Close(object sender, CanExecuteRoutedEventArgs e)
         {
