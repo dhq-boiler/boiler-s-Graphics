d5ca26ff1663b9d941157ab6c60dd01f630a9043
dhq_boiler <dhq_boiler@live.jp>
2021-10-26 18:23:40 +0900

    bugfix

--- boilersGraphics/ViewModels/StraightConnectorViewModel.cs
+++ boilersGraphics/ViewModels/StraightConnectorViewModel.cs
@@ -2,7 +2,9 @@
 using Prism.Ioc;
 using Prism.Services.Dialogs;
 using Prism.Unity;
+using Reactive.Bindings;
 using System;
+using System.Reactive.Linq;
 using System.Windows;
 using System.Windows.Shapes;
 
@@ -24,8 +26,17 @@ namespace boilersGraphics.ViewModels
             : base()
         {
             AddPoints(diagramViewModel, p1, p2);
+            P1X = Observable.Return(p1.X).ToReactiveProperty();
+            P1Y = Observable.Return(p1.Y).ToReactiveProperty();
+            P2X = Observable.Return(p2.X).ToReactiveProperty();
+            P2Y = Observable.Return(p2.Y).ToReactiveProperty();
             InitIsSelectedOnSnapPoints();
         }
+
+        public ReactiveProperty<double> P1X { get; set; }
+        public ReactiveProperty<double> P1Y { get; set; }
+        public ReactiveProperty<double> P2X { get; set; }
+        public ReactiveProperty<double> P2Y { get; set; }
 
         public override Type GetViewType()
         {
@@ -54,8 +65,12 @@ namespace boilersGraphics.ViewModels
             if (result != null && result.Result == ButtonResult.OK)
             {
                 var viewModel = result.Parameters.GetValue<StraightConnectorViewModel>("ViewModel");
-                this.Points[0] = viewModel.Points[0];
-                this.Points[1] = viewModel.Points[1];
+                this.Points[0] = new Point(viewModel.P1X.Value, viewModel.P1Y.Value);
+                this.Points[1] = new Point(viewModel.P2X.Value, viewModel.P2Y.Value);
+                this.SnapPoint0VM.Value.Left.Value = viewModel.P1X.Value;
+                this.SnapPoint0VM.Value.Top.Value = viewModel.P1Y.Value;
+                this.SnapPoint1VM.Value.Left.Value = viewModel.P2X.Value;
+                this.SnapPoint1VM.Value.Top.Value = viewModel.P2Y.Value;
             }
         }
 
