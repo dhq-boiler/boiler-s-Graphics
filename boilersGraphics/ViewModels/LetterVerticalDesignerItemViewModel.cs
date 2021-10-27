5b34a1b5eabae5047bcc27d3d236deb9cbc80cc6
dhq_boiler <dhq_boiler@live.jp>
2021-10-27 16:46:43 +0900

    refactor

--- boilersGraphics/ViewModels/LetterVerticalDesignerItemViewModel.cs
+++ boilersGraphics/ViewModels/LetterVerticalDesignerItemViewModel.cs
@@ -1,79 +1,19 @@
 ﻿using boilersGraphics.Controls;
 using boilersGraphics.Extensions;
-using boilersGraphics.Models;
 using boilersGraphics.Views;
 using Prism.Ioc;
 using Prism.Services.Dialogs;
 using Prism.Unity;
-using Reactive.Bindings;
-using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows;
 using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace boilersGraphics.ViewModels
 {
-    public class LetterVerticalDesignerItemViewModel : DesignerItemViewModelBase, ILetterDesignerItemViewModel
+    public class LetterVerticalDesignerItemViewModel : AbstractLetterDesignerItemViewModel, ILetterDesignerItemViewModel
     {
-        private bool _LetterSettingDialogIsOpen = false;
-        private string _LetterString = string.Empty;
-        private FontFamilyEx _SelectedFontFamily;
-        private bool _IsBold;
-        private bool _IsItalic;
-        private int _FontSize;
-        private bool _AutoLineBreak;
-
-        public bool LetterSettingDialogIsOpen
-        {
-            get { return _LetterSettingDialogIsOpen; }
-            set { SetProperty(ref _LetterSettingDialogIsOpen, value); }
-        }
-
-        public string LetterString
-        {
-            get { return _LetterString; }
-            set { SetProperty(ref _LetterString, value); }
-        }
-
-        public FontFamilyEx SelectedFontFamily
-        {
-            get { return _SelectedFontFamily; }
-            set { SetProperty(ref _SelectedFontFamily, value); }
-        }
-
-        public bool IsBold
-        {
-            get { return _IsBold; }
-            set { SetProperty(ref _IsBold, value); }
-        }
-
-        public bool IsItalic
-        {
-            get { return _IsItalic; }
-            set { SetProperty(ref _IsItalic, value); }
-        }
-
-        public int FontSize
-        {
-            get { return _FontSize; }
-            set { SetProperty(ref _FontSize, value); }
-        }
-
-        public bool AutoLineBreak
-        {
-            get { return _AutoLineBreak; }
-            set { SetProperty(ref _AutoLineBreak, value); }
-        }
-
-        public event EventHandler LetterSettingDialogClose;
-
-        public ReactiveCommand GotFocusCommand { get; } = new ReactiveCommand();
-        public ReactiveCommand LostFocusCommand { get; } = new ReactiveCommand();
-
-        public override bool SupportsPropertyDialog => false;
+        public override bool SupportsPropertyDialog => true;
 
         public LetterVerticalDesignerItemViewModel(int id, DiagramViewModel parent, double left, double top)
             : base(id, parent, left, top)
@@ -87,85 +27,7 @@ namespace boilersGraphics.ViewModels
             Init();
         }
 
-        private void Init()
-        {
-            GotFocusCommand.Subscribe(x =>
-            {
-                if (!LetterSettingDialogIsOpen)
-                {
-                    var dialogService = new DialogService((App.Current as PrismApplication).Container as IContainerExtension);
-                    IDialogResult result = null;
-                    dialogService.Show(nameof(LetterVerticalSetting), new DialogParameters() { { "ViewModel", this } }, ret => result = ret);
-                    var designerCanvas = App.Current.MainWindow.GetChildOfType<DesignerCanvas>();
-                    designerCanvas.Focus();
-                    LetterSettingDialogIsOpen = true;
-                }
-            })
-            .AddTo(_CompositeDisposable);
-            LostFocusCommand.Subscribe(x =>
-            {
-                if (LetterSettingDialogIsOpen)
-                {
-                    CloseLetterSettingDialog();
-                }
-            })
-            .AddTo(_CompositeDisposable);
-            this.ShowConnectors = false;
-            this.ObserveProperty(x => x.LetterString)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            this.ObserveProperty(x => x.SelectedFontFamily)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            this.ObserveProperty(x => x.IsBold)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            this.ObserveProperty(x => x.IsItalic)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            this.ObserveProperty(x => x.FontSize)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            this.ObserveProperty(x => x.Width.Value)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            this.ObserveProperty(x => x.AutoLineBreak)
-                .Subscribe(_ => RenderLetter())
-                .AddTo(_CompositeDisposable);
-            EnablePathGeometryUpdate.Value = true;
-        }
-
-        public void CloseLetterSettingDialog()
-        {
-            if (LetterSettingDialogIsOpen)
-            {
-                LetterSettingDialogClose?.Invoke(this, new EventArgs());
-                LetterSettingDialogIsOpen = false;
-            }
-        }
-
-        private void RenderLetter()
-        {
-            if (SelectedFontFamily != null && FontSize > 0)
-            {
-                var fontStyle = IsItalic ? FontStyles.Italic : FontStyles.Normal;
-                var fontWeight = IsBold ? FontWeights.Bold : FontWeights.Normal;
-                var typeface = new Typeface(new FontFamilyEx(SelectedFontFamily.FamilyName), fontStyle, fontWeight, FontStretches.Normal);
-                GlyphTypeface glyphTypeface;
-                if (!typeface.TryGetGlyphTypeface(out glyphTypeface))
-                    return;
-                if (AutoLineBreak)
-                    WithLineBreak(glyphTypeface);
-                else
-                    WithoutLineBreak(glyphTypeface);
-            }
-            else
-            {
-                PathGeometry.Value = new PathGeometry();
-            }
-        }
-
-        private void WithLineBreak(GlyphTypeface glyphTypeface)
+        public override void WithLineBreak(GlyphTypeface glyphTypeface)
         {
             //refresh path geometry
             PathGeometry.Value = new PathGeometry();
@@ -266,7 +128,7 @@ namespace boilersGraphics.ViewModels
             }
         }
 
-        private void WithoutLineBreak(GlyphTypeface glyphTypeface)
+        public override void WithoutLineBreak(GlyphTypeface glyphTypeface)
         {
             var l = new List<PathGeometry>();
             var list = new List<PathGeometry>();
@@ -322,25 +184,6 @@ namespace boilersGraphics.ViewModels
             }
         }
 
-        public override PathGeometry CreateGeometry()
-        {
-            RenderLetter();
-            return PathGeometry.Value;
-        }
-
-        public override PathGeometry CreateGeometry(double angle)
-        {
-            RenderLetter();
-            var rotatePathGeometry = PathGeometry.Value.Clone();
-            rotatePathGeometry.Transform = new RotateTransform(angle, this.CenterPoint.Value.X, this.CenterPoint.Value.Y);
-            return rotatePathGeometry;
-        }
-
-        public override Type GetViewType()
-        {
-            return typeof(Path);
-        }
-
         #region IClonable
 
         public override object Clone()
@@ -366,11 +209,33 @@ namespace boilersGraphics.ViewModels
             return clone;
         }
 
+        #endregion //IClonable
+
         public override void OpenPropertyDialog()
         {
-            throw new NotImplementedException();
+            var dialogService = new DialogService((App.Current as PrismApplication).Container as IContainerExtension);
+            IDialogResult result = null;
+            dialogService.ShowDialog(nameof(DetailLetter), new DialogParameters() { { "ViewModel", (AbstractLetterDesignerItemViewModel)this.Clone() } }, ret => result = ret);
+            if (result != null && result.Result == ButtonResult.OK)
+            {
+                var viewModel = result.Parameters.GetValue<AbstractLetterDesignerItemViewModel>("ViewModel");
+                this.Left.Value = viewModel.Left.Value;
+                this.Top.Value = viewModel.Top.Value;
+                this.Width.Value = viewModel.Width.Value;
+                this.Height.Value = viewModel.Height.Value;
+                this.CenterX.Value = viewModel.CenterX.Value;
+                this.CenterY.Value = viewModel.CenterY.Value;
+            }
         }
 
-        #endregion //IClonable
+        public override void OpenSettingDialog()
+        {
+            var dialogService = new DialogService((App.Current as PrismApplication).Container as IContainerExtension);
+            IDialogResult result = null;
+            dialogService.Show(nameof(LetterVerticalSetting), new DialogParameters() { { "ViewModel", this } }, ret => result = ret);
+            var designerCanvas = App.Current.MainWindow.GetChildOfType<DesignerCanvas>();
+            designerCanvas.Focus();
+            LetterSettingDialogIsOpen = true;
+        }
     }
 }
