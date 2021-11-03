0bf410766d64c04b21715900b5bfa226eb77112f
dhq_boiler <dhq_boiler@live.jp>
2021-11-03 18:38:03 +0900

    implemented previewing autosave files

---
+++ boilersGraphics/Converters/PreviewConverter.cs
@@ -0,0 +1,25 @@
+﻿using boilersGraphics.UserControls;
+using boilersGraphics.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace boilersGraphics.Converters
+{
+    class PreviewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var diagramViewModel = new DiagramViewModel(App.Current.MainWindow.DataContext as MainWindowViewModel, 1000, 1000);
+            diagramViewModel.Preview(value as string);
+            var previewDiagramControl = new PreviewDiagramControl();
+            previewDiagramControl.DataContext = diagramViewModel;
+            return previewDiagramControl;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
