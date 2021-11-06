1721120cff351273b1ed72f83a0963f660077e60
dhq_boiler <dhq_boiler@live.jp>
2021-11-06 14:40:49 +0900

    implemented the remaining statistical items

--- boilersGraphics/Models/Statistics.cs
+++ boilersGraphics/Models/Statistics.cs
@@ -16,6 +16,54 @@ namespace boilersGraphics.Models
         private int _numberOfDrawsOfTheImageFileTool;
         private int _numberOfDrawsOfTheLetterTool;
         private int _numberOfDrawsOfTheVerticalLetterTool;
+        private int _numberOfDrawsOfPolygonTool;
+        private int _numberOfDrawsOfBezierCurveTool;
+        private int _numberOfSnapPointToolInstallations;
+        private int _brushToolDrawCount;
+        private int _numberOfTimesTheEraserToolHasBeenUsed;
+        private int _numberOfTimesSaved;
+        private int _numberOfTimesYouHaveNamedAndSaved;
+        private int _numberOfExports;
+        private int _numberOfJpegExports;
+        private int _numberOfPngExports;
+        private int _numberOfGifExports;
+        private int _numberOfBmpExports;
+        private int _numberOfTiffExports;
+        private int _numberOfWmpExports;
+        private int _numberOfTimesGrouped;
+        private int _numberOfUngrouped;
+        private int _numberOfMovesToTheFrontend;
+        private int _numberOfMovesToTheFront;
+        private int _numberOfMovesToTheBack;
+        private int _numberOfMovesToTheBackend;
+        private int _numberOfTopAlignment;
+        private int _numberOfTimesTheTopAndBottomAreCentered;
+        private int _numberOfBottomAlignment;
+        private int _numberOfLeftAlignment;
+        private int _numberOfTimesLeftAndRightCentered;
+        private int _numberOfRightAlignment;
+        private int _numberOfTimesAlignedLeftAndRight;
+        private int _numberOfTimesAlignedUpAndDown;
+        private int _numberOfTimesToMatchTheWidth;
+        private int _numberOfTimesToMatchTheHeight;
+        private int _numberOfUnions;
+        private int _numberOfIntersects;
+        private int _numberOfXors;
+        private int _numberOfExcludes;
+        private int _numberOfCuts;
+        private int _numberOfCopies;
+        private int _numberOfPasted;
+        private int _numberOfUndos;
+        private int _numberOfRedoes;
+        private int _numberOfTimesAutomaticallySaved;
+        private int _numberOfNewlyCreatedLayers;
+        private int _numberOfDeletedLayers;
+        private int _numberOfTimesTheItemWasDrawn;
+        private int _numberOfTimesTheItemWasDeleted;
+        private int _numberOfLogLevelChanges;
+        private int _numberOfTimesTheVersionInformationDialogWasDisplayed;
+        private int _numberOfTimesTheApplicationLogWasDisplayed;
+
 
         /// <summary>
         /// boiler's Graphics 起動回数
@@ -78,39 +126,508 @@ namespace boilersGraphics.Models
             set => SetProperty(ref _numberOfDrawsOfTheStraightLineTool, value);
         }
 
-        [Column("NumberOfDrawsOfTheRectangleTool", "INTEGER", 7)]
+        [Column("NumberOfDrawsOfTheRectangleTool", "INTEGER", 8)]
         public int NumberOfDrawsOfTheRectangleTool
         {
             get => _numberOfDrawsOfTheRectangleTool;
             set => SetProperty(ref _numberOfDrawsOfTheRectangleTool, value);
         }
 
-        [Column("NumberOfDrawsOfTheEllipseTool", "INTEGER", 7)]
+        [Column("NumberOfDrawsOfTheEllipseTool", "INTEGER", 9)]
         public int NumberOfDrawsOfTheEllipseTool
         {
             get => _numberOfDrawsOfTheEllipseTool;
             set => SetProperty(ref _numberOfDrawsOfTheEllipseTool, value);
         }
 
-        [Column("NumberOfDrawsOfTheImageFileTool", "INTEGER", 7)]
+        [Column("NumberOfDrawsOfTheImageFileTool", "INTEGER", 10)]
         public int NumberOfDrawsOfTheImageFileTool
         {
             get => _numberOfDrawsOfTheImageFileTool;
             set => SetProperty(ref _numberOfDrawsOfTheImageFileTool, value);
         }
 
-        [Column("NumberOfDrawsOfTheLetterTool", "INTEGER", 7)]
+        [Column("NumberOfDrawsOfTheLetterTool", "INTEGER", 11)]
         public int NumberOfDrawsOfTheLetterTool
         {
             get => _numberOfDrawsOfTheLetterTool;
             set => SetProperty(ref _numberOfDrawsOfTheLetterTool, value);
         }
 
-        [Column("NumberOfDrawsOfTheVerticalLetterTool", "INTEGER", 7)]
+        [Column("NumberOfDrawsOfTheVerticalLetterTool", "INTEGER", 12)]
         public int NumberOfDrawsOfTheVerticalLetterTool
         {
             get => _numberOfDrawsOfTheVerticalLetterTool;
             set => SetProperty(ref _numberOfDrawsOfTheVerticalLetterTool, value);
-        }
+		}
+
+        /// <summary>
+		/// 多角形ツールの描画回数
+		/// </summary>
+		[Column("NumberOfDrawsOfPolygonTool", "INTEGER", 13)]
+		public int NumberOfDrawsOfPolygonTool
+		{
+			get => _numberOfDrawsOfPolygonTool;
+			set => SetProperty(ref _numberOfDrawsOfPolygonTool, value);
+		}
+
+		///<summary>
+		/// ベジエ曲線ツールの描画回数
+		/// </summary>
+		[Column("NumberOfDrawsOfBezierCurveTool", "INTEGER", 14)]
+		public int NumberOfDrawsOfBezierCurveTool
+		{
+			get => _numberOfDrawsOfBezierCurveTool;
+			set => SetProperty(ref _numberOfDrawsOfBezierCurveTool, value);
+		}
+
+		///<summary>
+		/// スナップポイントツールの設置回数
+		/// </summary>
+		[Column("NumberOfSnapPointToolInstallations", "INTEGER", 15)]
+		public int NumberOfSnapPointToolInstallations
+		{
+			get => _numberOfSnapPointToolInstallations;
+			set => SetProperty(ref _numberOfSnapPointToolInstallations, value);
+		}
+
+		///<summary>
+		/// ブラシツールの描画回数
+		/// </summary>
+		[Column("BrushToolDrawCount", "INTEGER", 16)]
+		public int BrushToolDrawCount
+		{
+			get => _brushToolDrawCount;
+			set => SetProperty(ref _brushToolDrawCount, value);
+		}
+
+		///<summary>
+		/// 消しゴムツールの使用回数
+		/// </summary>
+		[Column("NumberOfTimesTheEraserToolHasBeenUsed", "INTEGER", 17)]
+		public int NumberOfTimesTheEraserToolHasBeenUsed
+		{
+			get => _numberOfTimesTheEraserToolHasBeenUsed;
+			set => SetProperty(ref _numberOfTimesTheEraserToolHasBeenUsed, value);
+		}
+
+		///<summary>
+		/// 上書き保存した回数
+		/// </summary>
+		[Column("NumberOfTimesSaved", "INTEGER", 18)]
+		public int NumberOfTimesSaved
+		{
+			get => _numberOfTimesSaved;
+			set => SetProperty(ref _numberOfTimesSaved, value);
+		}
+
+		///<summary>
+		/// 名前を付けて保存した回数
+		/// </summary>
+		[Column("NumberOfTimesYouHaveNamedAndSaved", "INTEGER", 19)]
+		public int NumberOfTimesYouHaveNamedAndSaved
+		{
+			get => _numberOfTimesYouHaveNamedAndSaved;
+			set => SetProperty(ref _numberOfTimesYouHaveNamedAndSaved, value);
+		}
+
+		///<summary>
+		/// エクスポートした回数
+		/// </summary>
+		[Column("NumberOfExports", "INTEGER", 20)]
+		public int NumberOfExports
+		{
+			get => _numberOfExports;
+			set => SetProperty(ref _numberOfExports, value);
+		}
+
+		///<summary>
+		/// Jpegエクスポートした回数
+		/// </summary>
+		[Column("NumberOfJpegExports", "INTEGER", 21)]
+		public int NumberOfJpegExports
+		{
+			get => _numberOfJpegExports;
+			set => SetProperty(ref _numberOfJpegExports, value);
+		}
+
+		///<summary>
+		/// PNGエクスポートした回数
+		/// </summary>
+		[Column("NumberOfPngExports", "INTEGER", 22)]
+		public int NumberOfPngExports
+		{
+			get => _numberOfPngExports;
+			set => SetProperty(ref _numberOfPngExports, value);
+		}
+
+		///<summary>
+		/// GIFエクスポートした回数
+		/// </summary>
+		[Column("NumberOfGifExports", "INTEGER", 23)]
+		public int NumberOfGifExports
+		{
+			get => _numberOfGifExports;
+			set => SetProperty(ref _numberOfGifExports, value);
+		}
+
+		///<summary>
+		/// BMPエクスポートした回数
+		/// </summary>
+		[Column("NumberOfBmpExports", "INTEGER", 24)]
+		public int NumberOfBmpExports
+		{
+			get => _numberOfBmpExports;
+			set => SetProperty(ref _numberOfBmpExports, value);
+		}
+
+		///<summary>
+		/// TIFFエクスポートした回数
+		/// </summary>
+		[Column("NumberOfTiffExports", "INTEGER", 25)]
+		public int NumberOfTiffExports
+		{
+			get => _numberOfTiffExports;
+			set => SetProperty(ref _numberOfTiffExports, value);
+		}
+
+		///<summary>
+		/// WMPエクスポートした回数
+		/// </summary>
+		[Column("NumberOfWmpExports", "INTEGER", 26)]
+		public int NumberOfWmpExports
+		{
+			get => _numberOfWmpExports;
+			set => SetProperty(ref _numberOfWmpExports, value);
+		}
+
+		///<summary>
+		/// グループ化した回数
+		/// </summary>
+		[Column("NumberOfTimesGrouped", "INTEGER", 27)]
+		public int NumberOfTimesGrouped
+		{
+			get => _numberOfTimesGrouped;
+			set => SetProperty(ref _numberOfTimesGrouped, value);
+		}
+
+		///<summary>
+		/// グループ化解除した回数
+		/// </summary>
+		[Column("NumberOfUngrouped", "INTEGER", 28)]
+		public int NumberOfUngrouped
+		{
+			get => _numberOfUngrouped;
+			set => SetProperty(ref _numberOfUngrouped, value);
+		}
+
+		///<summary>
+		/// 最前面へ移動した回数
+		/// </summary>
+		[Column("NumberOfMovesToTheFrontend", "INTEGER", 29)]
+		public int NumberOfMovesToTheFrontend
+		{
+			get => _numberOfMovesToTheFrontend;
+			set => SetProperty(ref _numberOfMovesToTheFrontend, value);
+		}
+
+		///<summary>
+		/// 前面へ移動した回数
+		/// </summary>
+		[Column("NumberOfMovesToTheFront", "INTEGER", 30)]
+		public int NumberOfMovesToTheFront
+		{
+			get => _numberOfMovesToTheFront;
+			set => SetProperty(ref _numberOfMovesToTheFront, value);
+		}
+
+		///<summary>
+		/// 背面へ移動した回数
+		/// </summary>
+		[Column("NumberOfMovesToTheBack", "INTEGER", 31)]
+		public int NumberOfMovesToTheBack
+		{
+			get => _numberOfMovesToTheBack;
+			set => SetProperty(ref _numberOfMovesToTheBack, value);
+		}
+
+		///<summary>
+		/// 最背面へ移動した回数
+		/// </summary>
+		[Column("NumberOfMovesToTheBackend", "INTEGER", 32)]
+		public int NumberOfMovesToTheBackend
+		{
+			get => _numberOfMovesToTheBackend;
+			set => SetProperty(ref _numberOfMovesToTheBackend, value);
+		}
+
+		///<summary>
+		/// 上揃えした回数
+		/// </summary>
+		[Column("NumberOfTopAlignment", "INTEGER", 33)]
+		public int NumberOfTopAlignment
+		{
+			get => _numberOfTopAlignment;
+			set => SetProperty(ref _numberOfTopAlignment, value);
+		}
+
+		///<summary>
+		/// 上下中央揃えした回数
+		/// </summary>
+		[Column("NumberOfTimesTheTopAndBottomAreCentered", "INTEGER", 34)]
+		public int NumberOfTimesTheTopAndBottomAreCentered
+		{
+			get => _numberOfTimesTheTopAndBottomAreCentered;
+			set => SetProperty(ref _numberOfTimesTheTopAndBottomAreCentered, value);
+		}
+
+		///<summary>
+		/// 下揃えした回数
+		/// </summary>
+		[Column("NumberOfBottomAlignment", "INTEGER", 35)]
+		public int NumberOfBottomAlignment
+		{
+			get => _numberOfBottomAlignment;
+			set => SetProperty(ref _numberOfBottomAlignment, value);
+		}
+		///<summary>
+		/// 左揃えした回数
+		/// </summary>
+		[Column("NumberOfLeftAlignment", "INTEGER", 36)]
+		public int NumberOfLeftAlignment
+		{
+			get => _numberOfLeftAlignment;
+			set => SetProperty(ref _numberOfLeftAlignment, value);
+		}
+
+		///<summary>
+		/// 左右中央揃えした回数
+		/// </summary>
+		[Column("NumberOfTimesLeftAndRightCentered", "INTEGER", 37)]
+		public int NumberOfTimesLeftAndRightCentered
+		{
+			get => _numberOfTimesLeftAndRightCentered;
+			set => SetProperty(ref _numberOfTimesLeftAndRightCentered, value);
+		}
+
+		///<summary>
+		/// 右揃えした回数
+		/// </summary>
+		[Column("NumberOfRightAlignment", "INTEGER", 38)]
+		public int NumberOfRightAlignment
+		{
+			get => _numberOfRightAlignment;
+			set => SetProperty(ref _numberOfRightAlignment, value);
+		}
+
+		///<summary>
+		/// 左右に整列した回数
+		/// </summary>
+		[Column("NumberOfTimesAlignedLeftAndRight", "INTEGER", 39)]
+		public int NumberOfTimesAlignedLeftAndRight
+		{
+			get => _numberOfTimesAlignedLeftAndRight;
+			set => SetProperty(ref _numberOfTimesAlignedLeftAndRight, value);
+		}
+
+		///<summary>
+		/// 上下に整列した回数
+		/// </summary>
+		[Column("NumberOfTimesAlignedUpAndDown", "INTEGER", 40)]
+		public int NumberOfTimesAlignedUpAndDown
+		{
+			get => _numberOfTimesAlignedUpAndDown;
+			set => SetProperty(ref _numberOfTimesAlignedUpAndDown, value);
+		}
+
+		///<summary>
+		/// 幅を合わせた回数
+		/// </summary>
+		[Column("NumberOfTimesToMatchTheWidth", "INTEGER", 41)]
+		public int NumberOfTimesToMatchTheWidth
+		{
+			get => _numberOfTimesToMatchTheWidth;
+			set => SetProperty(ref _numberOfTimesToMatchTheWidth, value);
+		}
+
+		///<summary>
+		/// 高さを合わせた回数
+		/// </summary>
+		[Column("NumberOfTimesToMatchTheHeight", "INTEGER", 42)]
+		public int NumberOfTimesToMatchTheHeight
+		{
+			get => _numberOfTimesToMatchTheHeight;
+			set => SetProperty(ref _numberOfTimesToMatchTheHeight, value);
+		}
+
+		///<summary>
+		/// Unionした回数
+		/// </summary>
+		[Column("NumberOfUnions", "INTEGER", 43)]
+		public int NumberOfUnions
+		{
+			get => _numberOfUnions;
+			set => SetProperty(ref _numberOfUnions, value);
+		}
+
+		///<summary>
+		/// Intersectした回数
+		/// </summary>
+		[Column("NumberOfIntersects", "INTEGER", 44)]
+		public int NumberOfIntersects
+		{
+			get => _numberOfIntersects;
+			set => SetProperty(ref _numberOfIntersects, value);
+		}
+
+		///<summary>
+		/// Xorした回数
+		/// </summary>
+		[Column("NumberOfXors", "INTEGER", 45)]
+		public int NumberOfXors
+		{
+			get => _numberOfXors;
+			set => SetProperty(ref _numberOfXors, value);
+		}
+
+		///<summary>
+		/// Excludeした回数
+		/// </summary>
+		[Column("NumberOfExcludes", "INTEGER", 46)]
+		public int NumberOfExcludes
+		{
+			get => _numberOfExcludes;
+			set => SetProperty(ref _numberOfExcludes, value);
+		}
+
+		///<summary>
+		/// 切り取りした回数
+		/// </summary>
+		[Column("NumberOfCuts", "INTEGER", 47)]
+		public int NumberOfCuts
+		{
+			get => _numberOfCuts;
+			set => SetProperty(ref _numberOfCuts, value);
+		}
+
+		///<summary>
+		/// コピーした回数
+		/// </summary>
+		[Column("NumberOfCopies", "INTEGER", 48)]
+		public int NumberOfCopies
+		{
+			get => _numberOfCopies;
+			set => SetProperty(ref _numberOfCopies, value);
+		}
+
+		///<summary>
+		/// 貼り付けした回数
+		/// </summary>
+		[Column("NumberOfPasted", "INTEGER", 49)]
+		public int NumberOfPasted
+		{
+			get => _numberOfPasted;
+			set => SetProperty(ref _numberOfPasted, value);
+		}
+
+		///<summary>
+		/// 元に戻した回数
+		/// </summary>
+		[Column("NumberOfUndos", "INTEGER", 50)]
+		public int NumberOfUndos
+		{
+			get => _numberOfUndos;
+			set => SetProperty(ref _numberOfUndos, value);
+		}
+
+		///<summary>
+		/// やり直しした回数
+		/// </summary>
+		[Column("NumberOfRedoes", "INTEGER", 51)]
+		public int NumberOfRedoes
+		{
+			get => _numberOfRedoes;
+			set => SetProperty(ref _numberOfRedoes, value);
+		}
+
+		///<summary>
+		/// 自動保存した回数
+		/// </summary>
+		[Column("NumberOfTimesAutomaticallySaved", "INTEGER", 52)]
+		public int NumberOfTimesAutomaticallySaved
+		{
+			get => _numberOfTimesAutomaticallySaved;
+			set => SetProperty(ref _numberOfTimesAutomaticallySaved, value);
+		}
+
+		///<summary>
+		/// 新規作成したレイヤー数
+		/// </summary>
+		[Column("NumberOfNewlyCreatedLayers", "INTEGER", 53)]
+		public int NumberOfNewlyCreatedLayers
+		{
+			get => _numberOfNewlyCreatedLayers;
+			set => SetProperty(ref _numberOfNewlyCreatedLayers, value);
+		}
+
+		///<summary>
+		/// 削除したレイヤー数
+		/// </summary>
+		[Column("NumberOfDeletedLayers", "INTEGER", 54)]
+		public int NumberOfDeletedLayers
+		{
+			get => _numberOfDeletedLayers;
+			set => SetProperty(ref _numberOfDeletedLayers, value);
+		}
+
+		///<summary>
+		/// アイテムを描画した回数
+		/// </summary>
+		[Column("NumberOfTimesTheItemWasDrawn", "INTEGER", 55)]
+		public int NumberOfTimesTheItemWasDrawn
+		{
+			get => _numberOfTimesTheItemWasDrawn;
+			set => SetProperty(ref _numberOfTimesTheItemWasDrawn, value);
+		}
+
+		///<summary>
+		/// アイテムを削除した回数
+		/// </summary>
+		[Column("NumberOfTimesTheItemWasDeleted", "INTEGER", 56)]
+		public int NumberOfTimesTheItemWasDeleted
+		{
+			get => _numberOfTimesTheItemWasDeleted;
+			set => SetProperty(ref _numberOfTimesTheItemWasDeleted, value);
+		}
+
+		///<summary>
+		/// ログレベルを変更した回数
+		/// </summary>
+		[Column("NumberOfLogLevelChanges", "INTEGER", 57)]
+		public int NumberOfLogLevelChanges
+		{
+			get => _numberOfLogLevelChanges;
+			set => SetProperty(ref _numberOfLogLevelChanges, value);
+		}
+
+		///<summary>
+		/// バージョン情報ダイアログを表示した回数
+		/// </summary>
+		[Column("NumberOfTimesTheVersionInformationDialogWasDisplayed", "INTEGER", 58)]
+		public int NumberOfTimesTheVersionInformationDialogWasDisplayed
+		{
+			get => _numberOfTimesTheVersionInformationDialogWasDisplayed;
+			set => SetProperty(ref _numberOfTimesTheVersionInformationDialogWasDisplayed, value);
+		}
+
+		///<summary>
+		/// アプリケーションログを表示した回数
+		/// </summary>
+		[Column("NumberOfTimesTheApplicationLogWasDisplayed", "INTEGER", 59)]
+		public int NumberOfTimesTheApplicationLogWasDisplayed
+		{
+			get => _numberOfTimesTheApplicationLogWasDisplayed;
+			set => SetProperty(ref _numberOfTimesTheApplicationLogWasDisplayed, value);
+		}
     }
 }
