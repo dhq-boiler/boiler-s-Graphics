a3df43227e5c82ec2a1d7dcf4de32fbd1e0ae68d
dhq_boiler <dhq_boiler@live.jp>
2021-11-24 17:34:10 +0900

    investigating

--- boilersGraphics.Test/UITests/ExportTest.cs
+++ boilersGraphics.Test/UITests/ExportTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using NLog;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using System.IO;
 using System.Reflection;
@@ -20,6 +21,8 @@ namespace boilersGraphics.Test.UITests
                 session.FindElementByAccessibilityId("Export").Click();
                 session.FindElementByAccessibilityId("FileName").SendKeys(exportFilePath);
                 session.FindElementByAccessibilityId("PerformExport").Click();
+                Thread.Sleep(1000);
+                LogManager.GetCurrentClassLogger().Info(exportFilePath);
                 Assert.That(File.Exists(exportFilePath), Is.EqualTo(true));
             }
             finally
@@ -31,28 +34,45 @@ namespace boilersGraphics.Test.UITests
         [Test]
         public void チェッカーパターンを読み込んでエクスポートする()
         {
+            LogManager.GetCurrentClassLogger().Info("A");
             var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var loadFilePath = $"{dir}\\XmlFiles\\checker_pattern.xml";
             var exportFilePath = $"{dir}\\ExportTest2.jpg";
 
             try
             {
+                LogManager.GetCurrentClassLogger().Info("B");
                 session.FindElementByAccessibilityId("Load").Click();
+                LogManager.GetCurrentClassLogger().Info("C");
                 session.FindElementByAccessibilityId("1").Click();
+                LogManager.GetCurrentClassLogger().Info("D");
                 session.Keyboard.SendKeys(Keys.Alt + "N" + Keys.Alt);
+                LogManager.GetCurrentClassLogger().Info("E");
                 session.Keyboard.SendKeys(Keys.Alt + "N" + Keys.Alt);
+                LogManager.GetCurrentClassLogger().Info("F");
                 session.FindElementByAccessibilityId("1148").SendKeys(loadFilePath);
+                LogManager.GetCurrentClassLogger().Info("G");
                 session.FindElementByName("開く(O)").Click();
 
+                LogManager.GetCurrentClassLogger().Info("H");
+
+
                 session.FindElementByAccessibilityId("Export").Click();
+                LogManager.GetCurrentClassLogger().Info("I");
                 session.FindElementByAccessibilityId("FileName").SendKeys(exportFilePath);
+                LogManager.GetCurrentClassLogger().Info("J");
                 session.FindElementByAccessibilityId("PerformExport").Click();
+                LogManager.GetCurrentClassLogger().Info("K");
                 Thread.Sleep(1000);
+                LogManager.GetCurrentClassLogger().Info("L");
                 Assert.That(File.Exists(exportFilePath), Is.EqualTo(true));
+                LogManager.GetCurrentClassLogger().Info("M");
             }
             finally
             {
+                LogManager.GetCurrentClassLogger().Info("N");
                 File.Delete(exportFilePath);
+                LogManager.GetCurrentClassLogger().Info("O");
             }
         }
     }
