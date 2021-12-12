de4e6987bea717ca50a395e78cfd383fd9a92c83
dhq_boiler <dhq_boiler@live.jp>
2021-12-12 16:38:56 +0900

    investigating

--- boilersGraphics.Test/UITests/ExportTest.cs
+++ boilersGraphics.Test/UITests/ExportTest.cs
@@ -20,6 +20,7 @@ namespace boilersGraphics.Test.UITests
             var exportFilePath = $"{dir}\\ExportTest.jpg";
 
             GetElementByAutomationID("Export").Click();
+            session.WindowHandles.Select(x => session.SwitchTo().Window(x)).ToList().ForEach(x => LogManager.GetCurrentClassLogger().Info("WindowTitle:" + x.Title));
             session.WindowHandles.Select(x => session.SwitchTo().Window(x)).First(x => x.Title.StartsWith("エクスポート")).SwitchTo();
             GetElementByAutomationID("filename").SendKeys(exportFilePath);
             GetElementByAutomationID("PerformExport").Click();
@@ -94,8 +95,9 @@ namespace boilersGraphics.Test.UITests
             LogManager.GetCurrentClassLogger().Info("I");
             TakeScreenShot("SCREENSHOT_I.png");
 
+            session.WindowHandles.Select(x => session.SwitchTo().Window(x)).ToList().ForEach(x => LogManager.GetCurrentClassLogger().Info("WindowTitle:" + x.Title));
             session.WindowHandles.Select(x => session.SwitchTo().Window(x)).First(x => x.Title.StartsWith("エクスポート")).SwitchTo();
-                
+
             GetElementByAutomationID("filename").SendKeys(exportFilePath);
                 
             LogManager.GetCurrentClassLogger().Info("J");
@@ -209,6 +211,7 @@ namespace boilersGraphics.Test.UITests
 
             TakeScreenShot("SCREENSHOT_PREVIEW.png");
 
+            session.WindowHandles.Select(x => session.SwitchTo().Window(x)).ToList().ForEach(x => LogManager.GetCurrentClassLogger().Info("WindowTitle:" + x.Title));
             session.WindowHandles.Select(x => session.SwitchTo().Window(x)).First(x => x.Title.StartsWith("エクスポート")).SwitchTo();
 
             GetElementBy(By.XPath("//Window[@ClassName=\"Window\"][@Name=\"エクスポート\"]/Custom[@ClassName=\"Export\"]/Image[@Name=\"Preview\"][@AutomationId=\"Preview\"]")).GetScreenshot().SaveAsFile(previewFilePath);
