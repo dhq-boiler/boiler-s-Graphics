1b14801d84038de2adb19dfb3f3b74f2ad5140d5
dhq_boiler <dhq_boiler@live.jp>
2021-12-18 15:23:03 +0900

    organize using

--- boilersGraphics/Helpers/Name.cs
+++ boilersGraphics/Helpers/Name.cs
@@ -1,11 +1,5 @@
-﻿using boilersGraphics.Models;
-using boilersGraphics.Properties;
+﻿using boilersGraphics.Properties;
 using boilersGraphics.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace boilersGraphics.Helpers
 {
