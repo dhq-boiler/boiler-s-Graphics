9d61a795bd520d84fb8238b1435a9731367a95e6
dhq_boiler <dhq_boiler@live.jp>
2022-01-27 18:54:18 +0900

    #68 fixed

--- boilersGraphics/Controls/DesignerCanvas.cs
+++ boilersGraphics/Controls/DesignerCanvas.cs
@@ -3,13 +3,9 @@ using boilersGraphics.Helpers;
 using boilersGraphics.Messenger;
 using boilersGraphics.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace boilersGraphics.Controls
 {
@@ -46,34 +42,6 @@ namespace boilersGraphics.Controls
             var position = e.GetPosition(this);
 
             (DataContext as DiagramViewModel).CurrentPoint = position;
-        }
-
-
-        protected override Size MeasureOverride(Size constraint)
-        {
-            Size size = new Size();
-
-            foreach (UIElement element in this.InternalChildren)
-            {
-                double left = Canvas.GetLeft(element);
-                double top = Canvas.GetTop(element);
-                left = double.IsNaN(left) ? 0 : left;
-                top = double.IsNaN(top) ? 0 : top;
-
-                //measure desired size for each child
-                element.Measure(constraint);
-
-                Size desiredSize = element.DesiredSize;
-                if (!double.IsNaN(desiredSize.Width) && !double.IsNaN(desiredSize.Height))
-                {
-                    size.Width = Math.Max(size.Width, left + desiredSize.Width);
-                    size.Height = Math.Max(size.Height, top + desiredSize.Height);
-                }
-            }
-            // add margin 
-            size.Width += 10;
-            size.Height += 10;
-            return size;
         }
 
         protected override void OnDrop(DragEventArgs e)
