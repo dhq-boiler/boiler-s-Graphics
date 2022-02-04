232f8889797a39196cc60817ce426b87df045798
dhq_boiler <dhq_boiler@live.jp>
2022-02-04 22:10:43 +0900

    #70 fixed

--- boilersGraphics/Converters/BrushToStringConverter.cs
+++ boilersGraphics/Converters/BrushToStringConverter.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using boilersGraphics.Exceptions;
+using System;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
@@ -16,7 +14,33 @@ namespace boilersGraphics.Converters
         {
             if (value == null) return DependencyProperty.UnsetValue;
             var color = (Brush)value;
-            return color.ToString();
+            if (color is SolidColorBrush)
+                return color.ToString();
+            else if (color is LinearGradientBrush lgb)
+            {
+                var ret = $"Linear{{";
+                foreach (var gs in lgb.GradientStops)
+                {
+                    ret += $"[{gs.Color.ToString()}, {gs.Offset}]";
+                    if (lgb.GradientStops.Last() != gs)
+                        ret += ", ";
+                }
+                ret += $"}}";
+                return ret;
+            }
+            else if (color is RadialGradientBrush rgb)
+            {
+                var ret = $"Radial{{";
+                foreach (var gs in rgb.GradientStops)
+                {
+                    ret += $"[{gs.Color.ToString()}, {gs.Offset}]";
+                    if (rgb.GradientStops.Last() != gs)
+                        ret += ", ";
+                }
+                ret += $"}}";
+                return ret;
+            }
+            throw new UnexpectedException($"Not supported Brush type:{color}");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
