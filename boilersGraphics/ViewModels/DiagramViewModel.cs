d986cc7889ac093f39af24b65c16ed8075c634ee
dhq_boiler <dhq_boiler@live.jp>
2022-04-18 22:24:28 +0900

    bugfix

--- boilersGraphics/ViewModels/DiagramViewModel.cs
+++ boilersGraphics/ViewModels/DiagramViewModel.cs
@@ -1093,10 +1093,24 @@ namespace boilersGraphics.ViewModels
                     var item1PathGeometry = item1.PathGeometryNoRotate.Value;
                     var item2PathGeometry = item2.PathGeometryNoRotate.Value;
 
-                    if (item1 is DesignerItemViewModelBase designerItem1 && item1.RotationAngle.Value != 0)
-                        item1PathGeometry = designerItem1.PathGeometryRotate.Value;
-                    if (item2 is DesignerItemViewModelBase designerItem2 && item2.RotationAngle.Value != 0)
-                        item2PathGeometry = designerItem2.PathGeometryRotate.Value;
+                    if (item1 is DesignerItemViewModelBase designerItem1)
+                    {
+                        if (item1.RotationAngle.Value != 0)
+                        {
+                            item1PathGeometry = designerItem1.PathGeometryRotate.Value;
+                        }
+                        item1PathGeometry = GeometryCreator.Translate(item1PathGeometry, designerItem1.Left.Value, designerItem1.Top.Value);
+                    }
+
+                    if (item2 is DesignerItemViewModelBase designerItem2)
+                    {
+                        if (item2.RotationAngle.Value != 0)
+                        {
+                            item2PathGeometry = designerItem2.PathGeometryRotate.Value;
+                        }
+                        item2PathGeometry = GeometryCreator.Translate(item2PathGeometry, designerItem2.Left.Value, designerItem2.Top.Value);
+                    }
+
 
                     CastToLetterAndSetTransform(item1, item2, item1PathGeometry, item2PathGeometry);
 
