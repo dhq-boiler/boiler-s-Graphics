ea0032c00f32f73cce7bc4a190422a4f1208e00c
dhq_boiler <dhq_boiler@live.jp>
2022-12-30 18:28:56 +0900

    try to fix

--- boilersGraphics/Properties/AssemblyInfo.cs
+++ boilersGraphics/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch + "." + ThisAssembly.Git.Commits)]
+[assembly: AssemblyVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch + "." + ThisAssembly.Git.Commits)]
 
 [assembly: AssemblyFileVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch + "." + ThisAssembly.Git.Commits)]
 
