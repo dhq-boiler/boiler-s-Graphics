3abfb35df6a21b5b431eb0a44613f686951ba51d
dhq_boiler <dhq_boiler@live.jp>
2023-01-03 23:34:52 +0900

    now implementing page objects

--- boilersGraphics.Test/UITests/PageObjects/MainWindow.cs
+++ boilersGraphics.Test/UITests/PageObjects/MainWindow.cs
@@ -24,9 +24,95 @@ namespace boilersGraphics.Test.UITests.PageObjects
 
         public WindowsElement Canvas => GetElementBy(By.XPath("//Pane[@Name=\"DesignerScrollViewer\"][@AutomationId=\"DesignerScrollViewer\"]/Thumb[@AutomationId=\"PART_DragThumb\"]"));
 
+        public void Click_PointerTool()
+        {
+            GetElementByName("pointer").Click();
+        }
+
+        public void Click_LassoTool()
+        {
+            GetElementByName("lasso").Click();
+        }
+
+        public void Click_StraightLineTool()
+        {
+            GetElementByName("straightline").Click();
+        }
+
+        public void Click_RectangleTool()
+        {
+            GetElementByName("rectangle").Click();
+        }
+
+        public void Click_EllipseTool()
+        {
+            GetElementByName("ellipse").Click();
+        }
+
+        public PictureOpenFileDialog Click_PictureTool()
+        {
+            GetElementByName("picture").Click();
+            return new PictureOpenFileDialog(Session);
+        }
+
+        public void Click_LetterTool()
+        {
+            GetElementByName("letter").Click();
+        }
+
+        public void Click_LetterVerticalTool()
+        {
+            GetElementByName("letter-vertical").Click();
+        }
+
+        public void Click_PolygonTool()
+        {
+            GetElementByName("polygon").Click();
+        }
+
+        public void Click_BezierTool()
+        {
+            GetElementByName("bezier").Click();
+        }
+
+        public void Click_SnapPointTool()
+        {
+            GetElementByName("snappoint").Click();
+        }
+
+        public void Click_BrushTool()
+        {
+            GetElementByName("brush").Click();
+        }
+
+        public void Click_EraserTool()
+        {
+            GetElementByName("eraser").Click();
+        }
+
         public void Click_SliceTool()
         {
             GetElementByName("slice").Click();
+        }
+
+        public void Click_PolyBezierTool()
+        {
+            GetElementByName("polybezier").Click();
+        }
+
+        public void Click_PieTool()
+        {
+            GetElementByName("pie").Click();
+        }
+
+        public void Click_DropperTool()
+        {
+            GetElementByName("dropper").Click();
+        }
+
+        public void Click_CanvasModifierTool()
+        {
+            GetElementByName("canvasModifier").Click();
         }
 
         private Actions action;
---
+++ boilersGraphics.Test/UITests/PageObjects/PictureOpenFileDialog.cs
@@ -0,0 +1,41 @@
+﻿using boilersE2E;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+
+namespace boilersGraphics.Test.UITests.PageObjects
+{
+    public class PictureOpenFileDialog : PageObjectBase
+    {
+        public PictureOpenFileDialog(WindowsDriver<WindowsElement> session)
+            : base(session)
+        { }
+
+        private Actions action;
+
+        public void InitializeActions()
+        {
+            action = new Actions(Session);
+        }
+
+        public void Focus_FileName()
+        {
+            action.SendKeys(Keys.Alt + "N" + Keys.Alt);
+        }
+
+        public void Input_FileName(string filename)
+        {
+            action.InputText(GetElementByAutomationID("1148"), filename);
+        }
+
+        public void Click_OpenButton()
+        {
+            action.Click(GetElementByAutomationID("1"));
+        }
+
+        public void Perform()
+        {
+            action.Perform();
+        }
+    }
+}
