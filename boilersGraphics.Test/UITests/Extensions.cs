15fee9e162c8f710999017f176221a50f525a3a5
dhq_boiler <dhq_boiler@live.jp>
2023-01-04 15:33:51 +0900

    organize usings

--- boilersGraphics.Test/UITests/Extensions.cs
+++ boilersGraphics.Test/UITests/Extensions.cs
@@ -1,12 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Interactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace boilersGraphics.Test.UITests
 {
