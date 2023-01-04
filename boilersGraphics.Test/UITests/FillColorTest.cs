df14cf540765db4dc3f17e11e74beb23699131e0
dhq_boiler <dhq_boiler@live.jp>
2023-01-04 22:44:04 +0900

    add tests

--- boilersGraphics.Test/UITests/FillColorTest.cs
+++ boilersGraphics.Test/UITests/FillColorTest.cs
@@ -101,25 +101,30 @@ namespace boilersGraphics.Test.UITests
             }
         }
 
+        [Test, Apartment(ApartmentState.STA)]
+        public void 塗りつぶし色を緑指定して矩形描画()
         {
             var mainwindowPO = new MainWindowPO(Session);
             var selectFillColorDialogPO = mainwindowPO.Click_SelectFillColorButton();
             selectFillColorDialogPO.Click_Solid();
-            selectFillColorDialogPO.HueDADA.Initialize();
-            selectFillColorDialogPO.HueDADA.MoveTo(35, 10);
-            selectFillColorDialogPO.HueDADA.ClickAndHold();
-            selectFillColorDialogPO.HueDADA.MoveTo(182, 10);
-            selectFillColorDialogPO.HueDADA.Release();
-            selectFillColorDialogPO.HueDADA.Perform();
-            selectFillColorDialogPO.ColorMapDADA.Initialize();
-            selectFillColorDialogPO.ColorMapDADA.MoveTo(1, 1);
-            selectFillColorDialogPO.ColorMapDADA.ClickAndHold();
-            for (int i = 0; i < byte.MaxValue; i++)
-            {
-                selectFillColorDialogPO.ColorMapDADA.MoveByOffset(1, 0);
-            }
-            selectFillColorDialogPO.ColorMapDADA.Release();
-            selectFillColorDialogPO.ColorMapDADA.Perform();
+            selectFillColorDialogPO.BlueDADA.Initialize();
+            selectFillColorDialogPO.BlueDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.BlueDADA.ClickAndHold();
+            selectFillColorDialogPO.BlueDADA.MoveTo(0, 10);
+            selectFillColorDialogPO.BlueDADA.Release();
+            selectFillColorDialogPO.BlueDADA.Perform();
+            selectFillColorDialogPO.GreenDADA.Initialize();
+            selectFillColorDialogPO.GreenDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.GreenDADA.ClickAndHold();
+            selectFillColorDialogPO.GreenDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.GreenDADA.Release();
+            selectFillColorDialogPO.GreenDADA.Perform();
+            selectFillColorDialogPO.RedDADA.Initialize();
+            selectFillColorDialogPO.RedDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.RedDADA.ClickAndHold();
+            selectFillColorDialogPO.RedDADA.MoveTo(0, 10);
+            selectFillColorDialogPO.RedDADA.Release();
+            selectFillColorDialogPO.RedDADA.Perform();
             selectFillColorDialogPO.Click_OK();
 
             mainwindowPO.Click_RectangleTool();
@@ -138,7 +143,53 @@ namespace boilersGraphics.Test.UITests
 
             using (var mat = new Mat(filename))
             {
-                PixelIsRed(mat, 150, 150);
+                PixelIsGreen(mat, 150, 150);
+            }
+        }
+
+        [Test, Apartment(ApartmentState.STA)]
+        public void 塗りつぶし色を青指定して矩形描画()
+        {
+            var mainwindowPO = new MainWindowPO(Session);
+            var selectFillColorDialogPO = mainwindowPO.Click_SelectFillColorButton();
+            selectFillColorDialogPO.Click_Solid();
+            selectFillColorDialogPO.BlueDADA.Initialize();
+            selectFillColorDialogPO.BlueDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.BlueDADA.ClickAndHold();
+            selectFillColorDialogPO.BlueDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.BlueDADA.Release();
+            selectFillColorDialogPO.BlueDADA.Perform();
+            selectFillColorDialogPO.GreenDADA.Initialize();
+            selectFillColorDialogPO.GreenDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.GreenDADA.ClickAndHold();
+            selectFillColorDialogPO.GreenDADA.MoveTo(0, 10);
+            selectFillColorDialogPO.GreenDADA.Release();
+            selectFillColorDialogPO.GreenDADA.Perform();
+            selectFillColorDialogPO.RedDADA.Initialize();
+            selectFillColorDialogPO.RedDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.RedDADA.ClickAndHold();
+            selectFillColorDialogPO.RedDADA.MoveTo(0, 10);
+            selectFillColorDialogPO.RedDADA.Release();
+            selectFillColorDialogPO.RedDADA.Perform();
+            selectFillColorDialogPO.Click_OK();
+
+            mainwindowPO.Click_RectangleTool();
+
+            mainwindowPO.InitializeActions();
+            mainwindowPO.MoveToElement(100, 100);
+            mainwindowPO.ClickAndHold();
+            for (int i = 1; i < 100; ++i)
+                mainwindowPO.MoveByOffset(1, 1);
+            mainwindowPO.Release();
+            mainwindowPO.Perform();
+
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filename = $"{dir}\\Canvas.jpg";
+            mainwindowPO.SaveCanvas(filename);
+
+            using (var mat = new Mat(filename))
+            {
+                PixelIsBlue(mat, 150, 150);
             }
         }
 
@@ -149,6 +200,24 @@ namespace boilersGraphics.Test.UITests
             Assert.That(pic.Item0, Is.EqualTo(0), "{0},{1}", y, x);
             Assert.That(pic.Item1, Is.EqualTo(0), "{0},{1}", y, x);
             Assert.That(pic.Item2, Is.EqualTo(255), "{0},{1}", y, x);
+        }
+
+        private void PixelIsGreen(Mat mat, int y, int x)
+        {
+            Vec3b pic = mat.At<Vec3b>(y, x);
+            Console.WriteLine($"(b, g, r) = ({pic.Item0}, {pic.Item1}, {pic.Item2})");
+            Assert.That(pic.Item0, Is.EqualTo(0), "{0},{1}", y, x);
+            Assert.That(pic.Item1, Is.EqualTo(255), "{0},{1}", y, x);
+            Assert.That(pic.Item2, Is.EqualTo(0), "{0},{1}", y, x);
+        }
+
+        private void PixelIsBlue(Mat mat, int y, int x)
+        {
+            Vec3b pic = mat.At<Vec3b>(y, x);
+            Console.WriteLine($"(b, g, r) = ({pic.Item0}, {pic.Item1}, {pic.Item2})");
+            Assert.That(pic.Item0, Is.EqualTo(255), "{0},{1}", y, x);
+            Assert.That(pic.Item1, Is.EqualTo(0), "{0},{1}", y, x);
+            Assert.That(pic.Item2, Is.EqualTo(0), "{0},{1}", y, x);
         }
     }
 }
