7d7884efb3510338ec7d560aaab23ec21d5264f4
dhq_boiler <dhq_boiler@live.jp>
2023-01-04 22:34:54 +0900

    add test

--- boilersGraphics.Test/UITests/FillColorTest.cs
+++ boilersGraphics.Test/UITests/FillColorTest.cs
@@ -12,6 +12,50 @@ namespace boilersGraphics.Test.UITests
     public class FillColorTest : E2ETest
     {
         [Test, Apartment(ApartmentState.STA)]
+        public void 塗りつぶし色をHueで赤指定して矩形描画()
+        {
+            var mainwindowPO = new MainWindowPO(Session);
+            var selectFillColorDialogPO = mainwindowPO.Click_SelectFillColorButton();
+            selectFillColorDialogPO.Click_Solid();
+            selectFillColorDialogPO.HueDADA.Initialize();
+            selectFillColorDialogPO.HueDADA.MoveTo(35, 10);
+            selectFillColorDialogPO.HueDADA.ClickAndHold();
+            selectFillColorDialogPO.HueDADA.MoveTo(182, 10);
+            selectFillColorDialogPO.HueDADA.Release();
+            selectFillColorDialogPO.HueDADA.Perform();
+            selectFillColorDialogPO.ColorMapDADA.Initialize();
+            selectFillColorDialogPO.ColorMapDADA.MoveTo(0, -1);
+            selectFillColorDialogPO.ColorMapDADA.ClickAndHold();
+            for (int i = 0; i <= byte.MaxValue; i++)
+            {
+                selectFillColorDialogPO.ColorMapDADA.MoveByOffset(1, 0);
+            }
+            selectFillColorDialogPO.ColorMapDADA.MoveByOffset(0, -1);
+            selectFillColorDialogPO.ColorMapDADA.Release();
+            selectFillColorDialogPO.ColorMapDADA.Perform();
+            selectFillColorDialogPO.Click_OK();
+
+            mainwindowPO.Click_RectangleTool();
+
+            mainwindowPO.InitializeActions();
+            mainwindowPO.MoveToElement(100, 100);
+            mainwindowPO.ClickAndHold();
+            for (int i = 1; i < 100; ++i)
+                mainwindowPO.MoveByOffset(1, 1);
+            mainwindowPO.Release();
+            mainwindowPO.Perform();
+
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filename = $"{dir}\\Canvas.jpg";
+            mainwindowPO.SaveCanvas(filename);
+
+            using (var mat = new Mat(filename))
+            {
+                PixelIsRed(mat, 150, 150);
+            }
+        }
+
+        [Test, Apartment(ApartmentState.STA)]
         public void 塗りつぶし色を赤指定して矩形描画()
         {
             var mainwindowPO = new MainWindowPO(Session);
