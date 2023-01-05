d76589f105e71dfb1be6a71646fcd054d51e1ace
dhq_boiler <dhq_boiler@live.jp>
2023-01-05 20:31:22 +0900

    implemented ConditionalEnvironmentVariableAttribute

---
+++ boilersGraphics.Test/UITests/EnvironmentTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace boilersGraphics.Test.UITests
+{
+    [TestFixture]
+    public class EnvironmentTest
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetDpiForSystem();
+
+        [ConditionalEnvironmentVariable("OnAzureDevOps")]
+        [Test]
+        public void DPI96であることを確認()
+        {
+            Assert.That(GetDpiForSystem(),  Is.EqualTo(96), "DPI==96");
+            Assert.That(Screen.PrimaryScreen.Bounds.Width, Is.EqualTo(1980), "Screen.PrimaryScreen.Bounds.Width==1980");
+            Assert.That(Screen.PrimaryScreen.Bounds.Height, Is.EqualTo(1080), "Screen.PrimaryScreen.Bounds.Height==1080");
+        }
+    }
+}
