0d58f746f43a8e17a161a8b46d252e569c32ef89
dhq_boiler <dhq_boiler@live.jp>
2023-01-14 20:05:06 +0900

    implemented StrokeStartLineCap, StrokeEndLineCap, StrokeDashArray of poly bezier

--- boilersGraphics/ViewModels/DetailPolyBezierViewModel.cs
+++ boilersGraphics/ViewModels/DetailPolyBezierViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using System.Windows.Media;
 
 namespace boilersGraphics.ViewModels
 {
     class DetailPolyBezierViewModel : DetailViewModelBase<PolyBezierViewModel>
     {
+        public override void SetProperties()
+        {
+            Properties.Add(new PropertyOptionsValueCombinationStruct<PolyBezierViewModel, PenLineCap>(ViewModel.Value, "StrokeStartLineCap", new PenLineCap[]
+            {
+                PenLineCap.Flat,
+                PenLineCap.Round,
+                PenLineCap.Square,
+                PenLineCap.Triangle,
+            }));
+            Properties.Add(new PropertyOptionsValueCombinationStruct<PolyBezierViewModel, PenLineCap>(ViewModel.Value, "StrokeEndLineCap", new PenLineCap[]
+            {
+                PenLineCap.Flat,
+                PenLineCap.Round,
+                PenLineCap.Square,
+                PenLineCap.Triangle,
+            }));
+            Properties.Add(new PropertyOptionsValueCombinationClass<PolyBezierViewModel, DoubleCollection>(ViewModel.Value, "StrokeDashArray", HorizontalAlignment.Left));
+        }
     }
 }
