e49130108d1ee53a2240ecccf85a450a125c5364
dhq_boiler <dhq_boiler@live.jp>
2023-01-14 18:29:29 +0900

    refactor

--- boilersGraphics/ViewModels/DetailRectangleViewModel.cs
+++ boilersGraphics/ViewModels/DetailRectangleViewModel.cs
@@ -7,10 +7,10 @@ namespace boilersGraphics.ViewModels
     {
         public override void SetProperties()
         {
-            Properties.Add(new PropertyOptionsValueCombinationStruct<NRectangleViewModel, System.Windows.Media.PenLineJoin>(ViewModel.Value, "PenLineJoin", new System.Windows.Media.PenLineJoin[] {
-                System.Windows.Media.PenLineJoin.Miter,
-                System.Windows.Media.PenLineJoin.Bevel,
-                System.Windows.Media.PenLineJoin.Round
+            Properties.Add(new PropertyOptionsValueCombinationStruct<NRectangleViewModel, PenLineJoin>(ViewModel.Value, "PenLineJoin", new PenLineJoin[] {
+                PenLineJoin.Miter,
+                PenLineJoin.Bevel,
+                PenLineJoin.Round
             }));
             Properties.Add(new PropertyOptionsValueCombinationClass<NRectangleViewModel, DoubleCollection>(ViewModel.Value, "StrokeDashArray", HorizontalAlignment.Left));
             Properties.Add(new PropertyOptionsValueCombinationStruct<NRectangleViewModel, double>(ViewModel.Value, "RadiusX", HorizontalAlignment.Right));
