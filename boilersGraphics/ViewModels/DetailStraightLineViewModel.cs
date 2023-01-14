68dc0a5a5aa38c1b91255591ec082870c99404c1
dhq_boiler <dhq_boiler@live.jp>
2023-01-14 19:10:16 +0900

    implemented StrokeStartLineCap, StrokeEndLineCap, StrokeDashArray of Straight line

--- boilersGraphics/ViewModels/DetailStraightLineViewModel.cs
+++ boilersGraphics/ViewModels/DetailStraightLineViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace boilersGraphics.ViewModels
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace boilersGraphics.ViewModels
 {
     class DetailStraightLineViewModel : DetailViewModelBase<StraightConnectorViewModel>
     {
+        public override void SetProperties()
+        {
+            Properties.Add(new PropertyOptionsValueCombinationStruct<StraightConnectorViewModel, PenLineCap>(ViewModel.Value, "StrokeStartLineCap", new PenLineCap[]
+            {
+                PenLineCap.Flat,
+                PenLineCap.Round,
+                PenLineCap.Square,
+                PenLineCap.Triangle,
+            }));
+            Properties.Add(new PropertyOptionsValueCombinationStruct<StraightConnectorViewModel, PenLineCap>(ViewModel.Value, "StrokeEndLineCap", new PenLineCap[]
+            {
+                PenLineCap.Flat,
+                PenLineCap.Round,
+                PenLineCap.Square,
+                PenLineCap.Triangle,
+            }));
+            Properties.Add(new PropertyOptionsValueCombinationClass<StraightConnectorViewModel, DoubleCollection>(ViewModel.Value, "StrokeDashArray", HorizontalAlignment.Left));
+        }
     }
 }
