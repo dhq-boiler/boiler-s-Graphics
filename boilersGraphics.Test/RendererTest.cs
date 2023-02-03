f316bdf4fe7884431921a9ee5c24ea3580c73254
dhq_boiler <dhq_boiler@live.jp>
2023-02-04 01:27:44 +0900

    fixed and added test

--- boilersGraphics.Test/RendererTest.cs
+++ boilersGraphics.Test/RendererTest.cs
@@ -34,6 +34,7 @@ namespace boilersGraphics.Test
             var dlgService = new Mock<IDialogService>();
             var mainWindowViewModel = new MainWindowViewModel(dlgService.Object);
             var viewModel = new DiagramViewModel(mainWindowViewModel);
+            viewModel.Initialize();
 
             var designerCanvas = new DesignerCanvas();
 
@@ -68,6 +69,7 @@ namespace boilersGraphics.Test
                 }
             }
         }
+
         [Test, Apartment(ApartmentState.STA)]
         public void 赤青白全体を撮影()
         {
@@ -266,6 +268,7 @@ namespace boilersGraphics.Test
             var dlgService = new Mock<IDialogService>();
             var mainWindowViewModel = new MainWindowViewModel(dlgService.Object);
             var viewModel = new DiagramViewModel(mainWindowViewModel);
+            viewModel.Initialize();
 
             var designerCanvas = new DesignerCanvas();
 
@@ -330,7 +333,7 @@ namespace boilersGraphics.Test
             designerCanvas.Children.Add(blueRect);
 
             var mock = new Mock<IVisualTreeHelper>();
-            mock.Setup(x => x.GetDescendantBounds(backgroundRect)).Returns(new Rect(0, 0, 10, 10));
+            mock.Setup(x => x.GetDescendantBounds(redRect)).Returns(new Rect(2, 1, 3, 3));
             var renderer = new Renderer(mock.Object);
             var rtb = renderer.Render(new Rect(2, 1, 3, 3), designerCanvas, viewModel, viewModel.BackgroundItem.Value);
 
@@ -343,6 +346,219 @@ namespace boilersGraphics.Test
                     Assert.That(mat.At<Vec3b>(y, x), Is.EqualTo(new Vec3b(0, 0, 255)));
                 }
             }
+        }
+
+        [Test, Apartment(ApartmentState.STA)]
+        public void 青を撮影()
+        {
+            boilersGraphics.App.IsTest = true;
+            var dlgService = new Mock<IDialogService>();
+            var mainWindowViewModel = new MainWindowViewModel(dlgService.Object);
+            var viewModel = new DiagramViewModel(mainWindowViewModel);
+            viewModel.Initialize();
+
+            var designerCanvas = new DesignerCanvas();
+
+            viewModel.BackgroundItem.Value = new BackgroundViewModel(viewModel);
+            viewModel.BackgroundItem.Value.Left.Value = 0;
+            viewModel.BackgroundItem.Value.Top.Value = 0;
+            viewModel.BackgroundItem.Value.Width.Value = 10;
+            viewModel.BackgroundItem.Value.Height.Value = 10;
+            viewModel.BackgroundItem.Value.FillBrush.Value = new SolidColorBrush(Colors.White);
+            viewModel.BackgroundItem.Value.EdgeThickness.Value = 0;
+
+            var redRectViewModel = new NRectangleViewModel();
+            redRectViewModel.Left.Value = 2;
+            redRectViewModel.Top.Value = 1;
+            redRectViewModel.Width.Value = 3;
+            redRectViewModel.Height.Value = 3;
+            redRectViewModel.FillBrush.Value = new SolidColorBrush(Colors.Red);
+            redRectViewModel.EdgeThickness.Value = 0;
+            viewModel.AddItemCommand.Execute(redRectViewModel);
+
+            var blueRectViewModel = new NRectangleViewModel();
+            blueRectViewModel.Left.Value = 3;
+            blueRectViewModel.Top.Value = 5;
+            blueRectViewModel.Width.Value = 6;
+            blueRectViewModel.Height.Value = 3;
+            blueRectViewModel.FillBrush.Value = new SolidColorBrush(Colors.Blue);
+            blueRectViewModel.EdgeThickness.Value = 0;
+            viewModel.AddItemCommand.Execute(blueRectViewModel);
+
+            var backgroundRect = new Rectangle()
+            {
+                DataContext = viewModel.BackgroundItem.Value,
+                Width = 10,
+                Height = 10,
+                Fill = new SolidColorBrush(Colors.White)
+            };
+            Canvas.SetLeft(backgroundRect, 0);
+            Canvas.SetTop(backgroundRect, 0);
+            designerCanvas.Children.Add(backgroundRect);
+
+            var redRect = new Rectangle()
+            {
+                DataContext = redRectViewModel,
+                Width = 3,
+                Height = 3,
+                Fill = new SolidColorBrush(Colors.Red)
+            };
+            Canvas.SetLeft(redRect, 2);
+            Canvas.SetTop(redRect, 1);
+            designerCanvas.Children.Add(redRect);
+
+
+            var blueRect = new Rectangle()
+            {
+                DataContext = blueRectViewModel,
+                Width = 6,
+                Height = 3,
+                Fill = new SolidColorBrush(Colors.Blue)
+            };
+            Canvas.SetLeft(blueRect, 3);
+            Canvas.SetTop(blueRect, 5);
+            designerCanvas.Children.Add(blueRect);
+
+            var mock = new Mock<IVisualTreeHelper>();
+            mock.Setup(x => x.GetDescendantBounds(blueRect)).Returns(new Rect(3, 5, 6, 3));
+            var renderer = new Renderer(mock.Object);
+            var rtb = renderer.Render(new Rect(3, 5, 6, 3), designerCanvas, viewModel, viewModel.BackgroundItem.Value);
+
+            using var mat = OpenCvSharpHelper.ToMat(rtb);
+            mat.SaveImage("red.png");
+            for (var y = 0; y < 3; y++)
+            {
+                for (var x = 0; x < 6; x++)
+                {
+                    Assert.That(mat.At<Vec3b>(y, x), Is.EqualTo(new Vec3b(255, 0, 0)));
+                }
+            }
+        }
+
+
+        [Test, Apartment(ApartmentState.STA)]
+        public void 青マージン１を撮影()
+        {
+            boilersGraphics.App.IsTest = true;
+            var dlgService = new Mock<IDialogService>();
+            var mainWindowViewModel = new MainWindowViewModel(dlgService.Object);
+            var viewModel = new DiagramViewModel(mainWindowViewModel);
+            viewModel.Initialize();
+
+            var designerCanvas = new DesignerCanvas();
+
+            viewModel.BackgroundItem.Value = new BackgroundViewModel(viewModel);
+            viewModel.BackgroundItem.Value.Left.Value = 0;
+            viewModel.BackgroundItem.Value.Top.Value = 0;
+            viewModel.BackgroundItem.Value.Width.Value = 10;
+            viewModel.BackgroundItem.Value.Height.Value = 10;
+            viewModel.BackgroundItem.Value.FillBrush.Value = new SolidColorBrush(Colors.White);
+            viewModel.BackgroundItem.Value.EdgeThickness.Value = 0;
+
+            var redRectViewModel = new NRectangleViewModel();
+            redRectViewModel.Left.Value = 2;
+            redRectViewModel.Top.Value = 1;
+            redRectViewModel.Width.Value = 3;
+            redRectViewModel.Height.Value = 3;
+            redRectViewModel.FillBrush.Value = new SolidColorBrush(Colors.Red);
+            redRectViewModel.EdgeThickness.Value = 0;
+            viewModel.AddItemCommand.Execute(redRectViewModel);
+
+            var blueRectViewModel = new NRectangleViewModel();
+            blueRectViewModel.Left.Value = 3;
+            blueRectViewModel.Top.Value = 5;
+            blueRectViewModel.Width.Value = 6;
+            blueRectViewModel.Height.Value = 3;
+            blueRectViewModel.FillBrush.Value = new SolidColorBrush(Colors.Blue);
+            blueRectViewModel.EdgeThickness.Value = 0;
+            viewModel.AddItemCommand.Execute(blueRectViewModel);
+
+            var backgroundRect = new Rectangle()
+            {
+                DataContext = viewModel.BackgroundItem.Value,
+                Width = 10,
+                Height = 10,
+                Fill = new SolidColorBrush(Colors.White)
+            };
+            Canvas.SetLeft(backgroundRect, 0);
+            Canvas.SetTop(backgroundRect, 0);
+            designerCanvas.Children.Add(backgroundRect);
+
+            var redRect = new Rectangle()
+            {
+                DataContext = redRectViewModel,
+                Width = 3,
+                Height = 3,
+                Fill = new SolidColorBrush(Colors.Red)
+            };
+            Canvas.SetLeft(redRect, 2);
+            Canvas.SetTop(redRect, 1);
+            designerCanvas.Children.Add(redRect);
+
+
+            var blueRect = new Rectangle()
+            {
+                DataContext = blueRectViewModel,
+                Width = 6,
+                Height = 3,
+                Fill = new SolidColorBrush(Colors.Blue)
+            };
+            Canvas.SetLeft(blueRect, 3);
+            Canvas.SetTop(blueRect, 5);
+            designerCanvas.Children.Add(blueRect);
+
+            var mock = new Mock<IVisualTreeHelper>();
+            mock.Setup(x => x.GetDescendantBounds(blueRect)).Returns(new Rect(3, 5, 6, 3));
+            var renderer = new Renderer(mock.Object);
+            var rtb = renderer.Render(new Rect(2, 4, 8, 5), designerCanvas, viewModel, viewModel.BackgroundItem.Value);
+
+            using var mat = OpenCvSharpHelper.ToMat(rtb);
+            mat.SaveImage("blue_margin_1.png");
+            
+            Assert.That(mat.At<Vec3b>(0, 0), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 1), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 2), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 3), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 4), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 5), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 6), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(0, 7), Is.EqualTo(new Vec3b(255, 255, 255)));
+            
+            Assert.That(mat.At<Vec3b>(1, 0), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(1, 1), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(1, 2), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(1, 3), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(1, 4), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(1, 5), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(1, 6), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(1, 7), Is.EqualTo(new Vec3b(255, 255, 255)));
+            
+            Assert.That(mat.At<Vec3b>(2, 0), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(2, 1), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(2, 2), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(2, 3), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(2, 4), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(2, 5), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(2, 6), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(2, 7), Is.EqualTo(new Vec3b(255, 255, 255)));
+            
+            Assert.That(mat.At<Vec3b>(3, 0), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(3, 1), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(3, 2), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(3, 3), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(3, 4), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(3, 5), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(3, 6), Is.EqualTo(new Vec3b(255, 0, 0)));
+            Assert.That(mat.At<Vec3b>(3, 7), Is.EqualTo(new Vec3b(255, 255, 255)));
+            
+            Assert.That(mat.At<Vec3b>(4, 0), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 1), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 2), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 3), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 4), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 5), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 6), Is.EqualTo(new Vec3b(255, 255, 255)));
+            Assert.That(mat.At<Vec3b>(4, 7), Is.EqualTo(new Vec3b(255, 255, 255)));
         }
     }
 }
