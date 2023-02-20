14bd8e40ebec11849265fa0710964edc5fac2eda
dhq_boiler <dhq_boiler@live.jp>
2023-02-20 18:41:03 +0900

    bugfix

--- boilersGraphics.Test/StringTest.cs
+++ boilersGraphics.Test/StringTest.cs
@@ -17,5 +17,21 @@ namespace boilersGraphics.Test
             var index = s.IndexOf("k");
             Assert.That(string.Concat(s.Skip(index).Take(s.Count() - index)), Is.EqualTo("klmnopqrstuvwxyz"));
         }
+
+        [Test]
+        public void Convert_ToBase64String()
+        {
+            const string s1 = "abc";
+            const string s2 = "def";
+            const string s3 = "abcdef";
+            var stringBuilder1 = new StringBuilder();
+            stringBuilder1.Append(Convert.ToBase64String(Encoding.UTF8.GetBytes(s1)));
+            stringBuilder1.Append(Convert.ToBase64String(Encoding.UTF8.GetBytes(s2)));
+
+            var stringBuilder2 = new StringBuilder();
+            stringBuilder2.Append(Convert.ToBase64String(Encoding.UTF8.GetBytes(s3)));
+
+            Assert.That(stringBuilder1.ToString(), Is.EqualTo(stringBuilder2.ToString()));
+        }
     }
 }
