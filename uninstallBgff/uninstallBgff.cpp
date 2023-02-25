d15f3d174c2324a99e135e096588fcfe93c006ed
dhq_boiler <dhq_boiler@live.jp>
2023-02-26 00:31:15 +0900

    create uninstallBgff

---
+++ uninstallBgff/uninstallBgff.cpp
@@ -0,0 +1,31 @@
+﻿// uninstallBgff.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <windows.h>
+#include "..\BgffThumbnailProvider\Dll.h"
+
+int main()
+{
+    // DLLをロードする
+    HMODULE dllHandle = LoadLibrary(L"BgffThumbnailProvider.dll");
+
+    // DLLに含まれる関数のアドレスを取得する
+    MyFunctionType myFunction = (MyFunctionType)GetProcAddress(dllHandle, "DllUnregisterServer");
+
+    // DLLに含まれる関数を呼び出す
+    myFunction();
+
+    // DLLをアンロードする
+    FreeLibrary(dllHandle);
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
