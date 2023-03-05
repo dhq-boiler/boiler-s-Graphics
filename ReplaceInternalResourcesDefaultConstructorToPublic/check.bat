7d57e0a3f6b7b45a859c169640b2799930b35f75
dhq_boiler <dhq_boiler@live.jp>
2023-03-05 21:13:09 +0900

    bugfix

--- ReplaceInternalResourcesDefaultConstructorToPublic/check.bat
+++ ReplaceInternalResourcesDefaultConstructorToPublic/check.bat
@@ -1,10 +1,17 @@
 
+echo off
+
+echo %~dp0
+
+echo %1
+
 IF DEFINED Release (
 	echo Release
-	echo %~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe ..\boilersGraphics\Properties\Resources.Designer.cs
-	%~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe ..\boilersGraphics\Properties\Resources.Designer.cs
+	echo %~dp0
+	echo %~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe %1\boilersGraphics\Properties\Resources.Designer.cs
+	%~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe %1\boilersGraphics\Properties\Resources.Designer.cs
 ) ELSE (
 	echo Debug
-	echo %~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe ..\boilersGraphics\Properties\Resources.Designer.cs
-	%~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe ..\boilersGraphics\Properties\Resources.Designer.cs
+	echo %~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe %1\boilersGraphics\Properties\Resources.Designer.cs
+	%~dp0ReplaceInternalResourcesDefaultConstructorToPublic.exe %1\boilersGraphics\Properties\Resources.Designer.cs
 )
