22a17ee3aeac892a316cf3876fd640ef03b800e9
dhq_boiler <dhq_boiler@live.jp>
2026-04-02 21:34:15 +0900

    fix: resolve drag move not updating visual position on canvas
    WPF Style Setter binding {Binding Top.Value} does not propagate
    PropertyChanged from R3 BindableReactiveProperty. Added
    CanvasPositionBehavior that subscribes to PropertyChanged directly
    and calls Canvas.SetLeft/SetTop. Also optimized DragThumb to set
    values directly during drag and record undo only on mouse up.

--- boilersGraphics/Controls/DragThumb.cs
+++ boilersGraphics/Controls/DragThumb.cs
@@ -1,6 +1,8 @@
-﻿using boilersGraphics.ViewModels;
+using boilersGraphics.AttachedProperties;
+using boilersGraphics.ViewModels;
 using NLog;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -14,6 +16,8 @@ namespace boilersGraphics.Controls;
 
 public class DragThumb : Thumb
 {
+    private Dictionary<DesignerItemViewModelBase, (double Left, double Top)> _dragStartPositions;
+
     public DragThumb()
     {
         DragDelta += DragThumb_DragDelta;
@@ -25,7 +29,18 @@ public class DragThumb : Thumb
     protected override void OnMouseDown(MouseButtonEventArgs e)
     {
         base.OnMouseDown(e);
-        Recorder.BeginRecode();
+
+        // Record start positions for undo/redo
+        var designerItem = DataContext as DesignerItemViewModelBase;
+        if (designerItem != null)
+        {
+            _dragStartPositions = new Dictionary<DesignerItemViewModelBase, (double, double)>();
+            var items = designerItem.SelectedItems.AsValueEnumerable().OfType<DesignerItemViewModelBase>().ToList();
+            foreach (var item in items)
+            {
+                _dragStartPositions[item] = (item.Left.Value, item.Top.Value);
+            }
+        }
     }
 
     protected override void OnMouseUp(MouseButtonEventArgs e)
@@ -33,10 +48,31 @@ public class DragThumb : Thumb
         base.OnMouseUp(e);
         (Application.Current.MainWindow.DataContext as MainWindowViewModel).CurrentOperation.Value = "";
         (Application.Current.MainWindow.DataContext as MainWindowViewModel).Details.Value = "";
-        Recorder.EndRecode();
 
-        var item = DataContext as DesignerItemViewModelBase;
-        LogManager.GetCurrentClassLogger().Info($"Move item {item.ShowPropertiesAndFields()}");
+        // Record undo/redo operations for the entire drag
+        if (_dragStartPositions != null)
+        {
+            Recorder.BeginRecode();
+            foreach (var kvp in _dragStartPositions)
+            {
+                var item = kvp.Key;
+                var startLeft = kvp.Value.Left;
+                var startTop = kvp.Value.Top;
+                var endLeft = item.Left.Value;
+                var endTop = item.Top.Value;
+
+                if (startLeft != endLeft || startTop != endTop)
+                {
+                    Recorder.Current.ExecuteSetProperty(item, "Left.Value", endLeft);
+                    Recorder.Current.ExecuteSetProperty(item, "Top.Value", endTop);
+                }
+            }
+            Recorder.EndRecode();
+            _dragStartPositions = null;
+        }
+
+        var item2 = DataContext as DesignerItemViewModelBase;
+        LogManager.GetCurrentClassLogger().Info($"Move item {item2.ShowPropertiesAndFields()}");
     }
 
     private void DragThumb_DragDelta(object sender, DragDeltaEventArgs e)
@@ -85,22 +121,29 @@ public class DragThumb : Thumb
                 if (double.IsNaN(left)) left = 0;
                 if (double.IsNaN(top)) top = 0;
 
+                double newLeft, newTop;
                 if (matrixTransform != null)
                 {
                     var dragDelta = new Point(e.HorizontalChange, e.VerticalChange);
                     dragDelta = matrixTransform.Transform(dragDelta);
-                    Recorder.Current.ExecuteSetProperty(item, "Left.Value", left + dragDelta.X);
-                    Recorder.Current.ExecuteSetProperty(item, "Top.Value", top + dragDelta.Y);
-                    (Application.Current.MainWindow.DataContext as MainWindowViewModel).Details.Value =
-                        $"(x, y) = ({item.Left.Value}, {item.Top.Value}) (x+, y+) = ({dragDelta.X}, {dragDelta.Y})";
+                    newLeft = left + dragDelta.X;
+                    newTop = top + dragDelta.Y;
                 }
                 else
                 {
-                    Recorder.Current.ExecuteSetProperty(item, "Left.Value", left + deltaHorizontal);
-                    Recorder.Current.ExecuteSetProperty(item, "Top.Value", top + deltaVertical);
-                    (Application.Current.MainWindow.DataContext as MainWindowViewModel).Details.Value =
-                        $"(x, y) = ({item.Left.Value}, {item.Top.Value}) (x+, y+) = ({deltaHorizontal}, {deltaVertical})";
+                    newLeft = left + deltaHorizontal;
+                    newTop = top + deltaVertical;
                 }
+
+                // Directly set values without OperationRecorder overhead
+                item.Left.Value = newLeft;
+                item.Top.Value = newTop;
+
+                // Directly update Canvas position for immediate visual feedback
+                CanvasPositionBehavior.UpdateCanvasPosition(item, newLeft, newTop);
+
+                (Application.Current.MainWindow.DataContext as MainWindowViewModel).Details.Value =
+                    $"(x, y) = ({newLeft}, {newTop})";
             }
 
             e.Handled = true;
